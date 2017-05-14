3ca70eff827b5dc556b1bda9bd9ed34e2c290bed
hall1467 <hall1467@umn.edu>
2017-05-14 17:41:23 -0500

    Updated script so that any download errors are logged

--- page_views/download_dumps_05_2016_thru_04_2017.sh
+++ page_views/download_dumps_05_2016_thru_04_2017.sh
@@ -1,12 +1,12 @@
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-05/ https://dumps.wikimedia.org/other/pageviews/2016/2016-05/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-06/ https://dumps.wikimedia.org/other/pageviews/2016/2016-06/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-07/ https://dumps.wikimedia.org/other/pageviews/2016/2016-07/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-08/ https://dumps.wikimedia.org/other/pageviews/2016/2016-08/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-09/ https://dumps.wikimedia.org/other/pageviews/2016/2016-09/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-10/ https://dumps.wikimedia.org/other/pageviews/2016/2016-10/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-11/ https://dumps.wikimedia.org/other/pageviews/2016/2016-11/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2016-12/ https://dumps.wikimedia.org/other/pageviews/2016/2016-12/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2017-01/ https://dumps.wikimedia.org/other/pageviews/2016/2017-01/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2017-02/ https://dumps.wikimedia.org/other/pageviews/2016/2017-02/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2017-03/ https://dumps.wikimedia.org/other/pageviews/2016/2017-03/ &&
-wget -r --no-parent --reject html -nd -P ../page_view_dumps/2017-04/ https://dumps.wikimedia.org/other/pageviews/2016/2017-04/
+wget -r -o wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-05/ https://dumps.wikimedia.org/other/pageviews/2016/2016-05/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-06/ https://dumps.wikimedia.org/other/pageviews/2016/2016-06/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-07/ https://dumps.wikimedia.org/other/pageviews/2016/2016-07/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-08/ https://dumps.wikimedia.org/other/pageviews/2016/2016-08/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-09/ https://dumps.wikimedia.org/other/pageviews/2016/2016-09/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-10/ https://dumps.wikimedia.org/other/pageviews/2016/2016-10/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-11/ https://dumps.wikimedia.org/other/pageviews/2016/2016-11/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2016-12/ https://dumps.wikimedia.org/other/pageviews/2016/2016-12/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2017-01/ https://dumps.wikimedia.org/other/pageviews/2017/2017-01/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2017-02/ https://dumps.wikimedia.org/other/pageviews/2017/2017-02/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2017-03/ https://dumps.wikimedia.org/other/pageviews/2017/2017-03/ &&
+wget -r -a wikipedia_page_view_download_error_log.txt -nv --no-parent --reject html --accept-regex "pageview" -nd -P ../wikipedia_page_view_dumps/2017-04/ https://dumps.wikimedia.org/other/pageviews/2017/2017-04/
