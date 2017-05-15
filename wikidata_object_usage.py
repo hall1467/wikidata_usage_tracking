da12ed50c149ab623b5ec7eb48697d326f48263b
Aaron Halfaker <aaron.halfaker@gmail.com>
2017-05-15 11:01:02 -0500

    Cleanup but still a mess

--- wikidata_object_usage.py
+++ wikidata_object_usage.py
@@ -12,119 +12,116 @@ import operator
 import requests
 import gzip
 import csv
+from collections import defaultdict
 
 
-# Global dictionaries to store aspect and page usage
-wikidata_object_aspect_usage = {}
-wikidata_object_page_usage = {}
-wikidata_object_page_usage_count = {}
-aggregated_aspect_usages = {}
-aggregated_page_usage_counts = {}
+class WikibaseClientUsage:
+	
+	def __init__(self, entity_id, aspect, page_ids):
+		self.entity_id = str(entity_id)
+		self.aspect = str(aspect)
+		self.page_ids = page_ids
+	
+	def __hash__(self):
+		return hash((self.entity_id, self.aspect, self.page_ids))
+	
+	@classmethodconverted = ast.literal_eval(record)
+	def from_record_string(cls, record)
+		converted = ast.literal_eval(record)
+		return cls(converted[1], converted[2], converted[3])
 
 
-# Extract Wikidata object usages in wiki for given SQL entry
-def extractObjectUsages(input_sql_entry):
-		if re.match(r"INSERT INTO `wbc_entity_usage` VALUES ", input_sql_entry):
+def extract_usages(sql_file_obj):
+	""" Extract Wikidata object usages in wiki for given SQL entry"""
+	for line in sql_file_obj:
+		if re.match(r"INSERT INTO `wbc_entity_usage` VALUES ", line):
 
 			# Remove "INSERT INTO `wbc_entity_usage` VALUES " and trailing semi-colon/space
-			input_sql_entry = input_sql_entry.strip("INSERT INTO `wbc_entity_usage` VALUES ").rstrip().rstrip(";")
+			input_sql_entry = line.strip(
+				"INSERT INTO `wbc_entity_usage` VALUES ").rstrip().rstrip(";")
 
 			# Split tuples
 			records = re.findall("\([^)]*\)", input_sql_entry)
 			for record in records:
-				# Cast component to tuple
-				converted = ast.literal_eval(record)
+				yield WikibaseClientUsage.from_record_string(record)
 
-				# Extract object, wiki page, and usage information from tuple
-				wikidata_object = converted[1]
-				wikidata_aspect = converted[2]
-				wiki_page = converted[3]
 
-				# Initialize aspect usage storage dictionary for object if not already done
-				if wikidata_object not in wikidata_object_aspect_usage:
-					wikidata_object_aspect_usage[wikidata_object] = {}
+class WikidataObjectUsage:
+	
+	def __init__(self):
+		# Global dictionaries to store aspect and page usage
+		self.aspect_usage = defaultdict(dict)
+		self.page_usage = defaultdict(dict)
+		self.page_usage_count = defaultdict(int)
+		self.aggregated_aspect_usages = defaultdict(dict)
+		self.aggregated_page_usage_counts = defaultdict(int)
 
-				# Initialize nested aspect usage storage dictionary for object if not already done
-				if wikidata_aspect not in wikidata_object_aspect_usage[wikidata_object]:
-					wikidata_object_aspect_usage[wikidata_object][wikidata_aspect] = 0
 
-				# Initialize page usage storage dictionary for object if not already done
-				if wikidata_object not in wikidata_object_page_usage_count:
-					wikidata_object_page_usage_count[wikidata_object] = 0
-					wikidata_object_page_usage[wikidata_object] = {}
+
 
 				# Update object usage storage dictionaries
-				wikidata_object_aspect_usage[wikidata_object][wikidata_aspect] += 1
+				self.aspect_usage[wikidata_object][wikidata_aspect] += 1
 				if wiki_page not in wikidata_object_page_usage[wikidata_object]:
-					wikidata_object_page_usage_count[wikidata_object] += 1
-					wikidata_object_page_usage[wikidata_object][wiki_page] = True
+					self.page_usage_count[wikidata_object] += 1
+					self.page_usage[wikidata_object][wiki_page] = True
 
 
-# Return wikidata object page usages in descending order of usages, with aspect 
-# usage information
-def getSortedObjectUsagesFromDump(wiki, date):
-	global wikidata_object_aspect_usage
-	wikidata_object_aspect_usage = {}
+	# Return wikidata object page usages in descending order of usages, with aspect 
+	# usage information
+	def getSortedObjectUsagesFromDump(self, wiki, date):
 
-	global wikidata_object_page_usage
-	wikidata_object_page_usage = {}
+		dump = requests.get("https://dumps.wikimedia.org/" + wiki + "/" + str(date) 
+			   + "/" + wiki + "-" + str(date) + "-wbc_entity_usage.sql.gz", 
+			   stream=True)
+		dump_file = gzip.open(dump.raw)
 
-	global wikidata_object_page_usage_count
-	wikidata_object_page_usage_count = {}
+		for entry in dump_file:
+			self.extractObjectUsages(entry.decode())
 
-	dump = requests.get("https://dumps.wikimedia.org/" + wiki + "/" + str(date) 
-		   + "/" + wiki + "-" + str(date) + "-wbc_entity_usage.sql.gz", 
-		   stream=True)
-	dump_file = gzip.open(dump.raw)
-
-	for entry in dump_file:
-		extractObjectUsages(entry.decode())
-
-	return sortUsages(wikidata_object_page_usage_count,
-		   wikidata_object_aspect_usage)
+		return self.sortUsages()
 
 
-# Returns object usage in descending order of page count, includes aspects
-def sortUsages(object_page_usage_count, object_aspect_usage):
-	sorted_object_page_usage_count = sorted(object_page_usage_count.items(),
-	                                        key=operator.itemgetter(1),
-	                                        reverse=True)
-	wikidata_usages = []
-	for wikidata_object in sorted_object_page_usage_count:
-		wikidata_usages.append({'wikidata_object' : wikidata_object[0],
-			                    'wiki_pages_used_by' : wikidata_object[1],
-			                    'aspects_used_by_pages' : object_aspect_usage[wikidata_object[0]]})
+	# Returns object usage in descending order of page count, includes aspects
+	def sortUsages(self):
+		sorted_object_page_usage_count = sorted(self.page_usage_count.items(),
+												key=operator.itemgetter(1),
+												reverse=True)
+		wikidata_usages = []
+		for wikidata_object in sorted_object_page_usage_count:
+			wikidata_usages.append({'wikidata_object' : wikidata_object[0],
+									'wiki_pages_used_by' : wikidata_object[1],
+									'aspects_used_by_pages' : self.aspect_usage[wikidata_object[0]]})
 
-	return wikidata_usages
+		return wikidata_usages
 
+	
+	def addToWikidataUsageAggregation(self, wikidata_usages):
+		'''Adds Wikidata usages to aggregate usages across Wikis'''
+		for object_dictionary in wikidata_usages:
+			if object_dictionary['wikidata_object'] not in self.aggregated_aspect_usages:
+				self.aggregated_aspect_usages[object_dictionary['wikidata_object']] = {}
+
+			for aspect_usage in object_dictionary['aspects_used_by_pages']:
+				if aspect_usage not in aggregated_aspect_usages[object_dictionary['wikidata_object']]:
+					aggregated_aspect_usages[object_dictionary['wikidata_object']][aspect_usage] = 0
+				aggregated_aspect_usages[object_dictionary['wikidata_object']][aspect_usage] += object_dictionary['aspects_used_by_pages'][aspect_usage]
+
+			if object_dictionary['wikidata_object'] not in aggregated_page_usage_counts:
+				aggregated_page_usage_counts[object_dictionary['wikidata_object']] = 0
+			aggregated_page_usage_counts[object_dictionary['wikidata_object']] += object_dictionary['wiki_pages_used_by']
 
 # Writes Wikidata usage statistics to a file
 def writeWikidataUsagesToFile(directory, object_type, wiki, wikidata_usages, date):
 	wiki_file = open(directory + "/" + wiki + "_" + object_type + 
-		             "_wikidata_usages_" + date + ".csv", "w")
+					 "_wikidata_usages_" + date + ".csv", "w")
 	csv_object = csv.writer(wiki_file)
 	csv_object.writerow(["Object ID", "Wiki Pages Used By", "Aspects Used by Pages"])
 	for object_dictionary in wikidata_usages:
 		csv_object.writerow([object_dictionary['wikidata_object'],
-			                object_dictionary['wiki_pages_used_by'],
-			                object_dictionary['aspects_used_by_pages']])
+							object_dictionary['wiki_pages_used_by'],
+							object_dictionary['aspects_used_by_pages']])
 	wiki_file.close()
 
-
-# Adds Wikidata usages to aggregate usages across Wikis
-def addToWikidataUsageAggregation(wikidata_usages):
-	for object_dictionary in wikidata_usages:
-		if object_dictionary['wikidata_object'] not in aggregated_aspect_usages:
-			aggregated_aspect_usages[object_dictionary['wikidata_object']] = {}
-
-		for aspect_usage in object_dictionary['aspects_used_by_pages']:
-			if aspect_usage not in aggregated_aspect_usages[object_dictionary['wikidata_object']]:
-				aggregated_aspect_usages[object_dictionary['wikidata_object']][aspect_usage] = 0
-			aggregated_aspect_usages[object_dictionary['wikidata_object']][aspect_usage] += object_dictionary['aspects_used_by_pages'][aspect_usage]
-
-		if object_dictionary['wikidata_object'] not in aggregated_page_usage_counts:
-			aggregated_page_usage_counts[object_dictionary['wikidata_object']] = 0
-		aggregated_page_usage_counts[object_dictionary['wikidata_object']] += object_dictionary['wiki_pages_used_by']
 
 
 # Reset Wikidata usage aggregation
