b4ceae8d0f50130fc1b20dc0a889843c81d50bbe
hall1467 <hall1467@umn.edu>
2017-05-22 13:24:39 -0500

    Updates

--- wbc_usage/utilities/determine_wikis.py
+++ wbc_usage/utilities/determine_wikis.py
@@ -12,16 +12,19 @@ Options:
     --debug                        Print debug logging to stderr
     --verbose                      Print dots and stuff to stderr      
 """
-# Code credit: https://github.com/WikiEducationFoundation/academic_classification/blob/master/pageclassifier/revgather.py
 
+import logging
 import mwapi
 import sys
-import logging
 import json
+
 
 import docopt
 
+
+
 logger = logging.getLogger(__name__)
+
 
 
 def main(argv=None):
@@ -35,22 +38,8 @@ def main(argv=None):
     run(verbose)
 
 # Contacts API to return list of wikis
+# Code credit: https://github.com/WikiEducationFoundation/academic_classification/blob/master/pageclassifier/revgather.py
 def run(verbose):
-
-    def pull_text_from_query_results(results):
-        wiki_names_list = []
-        results = results['sitematrix']
-        for entry in results:
-            if entry == 'count':
-                continue
-            if entry == 'specials':
-                for special_entry in results[entry]:
-                    wiki_names_list.append({"dbname" : special_entry['dbname']})
-                continue
-            for wiki in results[entry]['site']:
-                wiki_names_list.append({"dbname" : wiki['dbname']})
-
-        return wiki_names_list
 
     session = mwapi.Session(
         'https://en.wikipedia.org',
@@ -61,12 +50,32 @@ def run(verbose):
         smsiteprop='dbname'
     )
 
-    for database_dictionary in pull_text_from_query_results(results):
+    for database_dictionary in extract_query_results(results):
 
         if verbose:
-            sys.stderr.write("Printing json for the database: " + database_dictionary['dbname'] + "\n")
+            sys.stderr.write("Printing json for the database: " +
+                database_dictionary['dbname'] + "\n")
             sys.stderr.flush()
 
         sys.stdout.write(json.dumps(database_dictionary) + "\n")
 
+
+# Code credit: https://github.com/WikiEducationFoundation/academic_classification/blob/master/pageclassifier/revgather.py
+def extract_query_results(results):
+    results = results['sitematrix']
+    for entry in results:
+        if entry == 'count':
+            continue
+        if entry == 'specials':
+            for special_entry in results[entry]:
+                yield ({
+                            "dbname" : special_entry['dbname'],
+                            # "wikiurl" : special_entry['url']
+                       })
+            continue
+        for wiki in results[entry]['site']:
+            yield {
+                      "dbname" : wiki['dbname'],
+                      # "wikiurl" : wiki['url']
+                  }
 
