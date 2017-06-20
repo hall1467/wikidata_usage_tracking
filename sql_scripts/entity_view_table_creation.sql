a6dd38bdf7d82ac47dcf7f4b6c9b9189a09aa3be
hall1467 <hall1467@umn.edu>
2017-06-20 10:15:41 -0500

    Fixed bug

--- sql_scripts/entity_view_table_creation.sql
+++ sql_scripts/entity_view_table_creation.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE s53311__wikidata_usage_and_value;
 
-CREATE TABLE entity_views(
+CREATE TABLE s53311__wikidata_usage_and_value.entity_views(
 	entity_id  varbinary(255),
 	page_views BIGINT
 );
