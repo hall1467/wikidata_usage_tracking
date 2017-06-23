30dd766731b979ec708f24b52cdfa6fd1c51ac49
hall1467 <hall1467@umn.edu>
2017-06-23 10:42:28 -0500

    Finished query

--- sql_scripts/postgres/queries/entity_proj_aspect_page_views_table_entity_views.sql
+++ sql_scripts/postgres/queries/entity_proj_aspect_page_views_table_entity_views.sql
@@ -1,7 +1,10 @@
 \COPY 
-	SELECT entity_id, page_id, avg(page_views)
+SELECT entity_id, page_views
+FROM (
+	SELECT entity_id, page_id, avg(page_views) AS page_views
 	FROM entity_proj_aspect_page_views
-	GROUP BY entity_id, page_id;
+	GROUP BY entity_id, page_id
+) AS entity_pages;
 TO '../../../../../wbc_entity_usage/usage_results/sql_queries/entity_proj_aspect_views_no_header.tsv';
 
 
