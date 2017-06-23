e0b184aa14d34ee290a8639aa046e391e9348395
hall1467 <hall1467@umn.edu>
2017-06-22 22:35:47 -0500

    Delete wikidata_entities_schema_creation.py

--- sql_scripts/postgres/creation/wikidata_entities_schema_creation.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-osm_db_connection = psycopg2.connect(dbname = "openstreetmap", user="hall")
-osm_db_connection_cursor = osm_db_connection.cursor()
-osm_db_connection_cursor.execute("CREATE SCHEMA wikidata_entities;")
-osm_db_connection.commit()
-osm_db_connection.close()
