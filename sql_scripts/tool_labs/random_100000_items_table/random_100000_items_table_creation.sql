919848e8bf9626a8a0965a1eb6cf9fc77f64b304
hall1467 <hall1467@umn.edu>
2017-06-28 22:07:05 -0500

    Referenced wrong table

--- sql_scripts/tool_labs/random_100000_items_table/random_100000_items_table_creation.sql
+++ sql_scripts/tool_labs/random_100000_items_table/random_100000_items_table_creation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE s53311__wikidata_usage_and_views_p.random_100000_items AS (
 	SELECT entity_id, page_views, latest_revision, page_namespace
-	FROM wikidatawiki_p.page
+	FROM s53311__wikidata_usage_and_views_p.entity_views_and_latest_revision
 	WHERE page_namespace = 0
 	ORDER BY RAND()
 	LIMIT 100000
