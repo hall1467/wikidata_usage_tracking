385c1363bc2a6ab2ca55de82ed1bb38950be4adc
hall1467 <hall1467@umn.edu>
2017-06-28 20:05:04 -0500

    New table

---
+++ sql_scripts/tool_labs/entity_views_and_latest_revision/entity_views_and_latest_revision_table_creation.sql
@@ -0,0 +1,5 @@
+CREATE TABLE s53311__wikidata_usage_and_views_p.entity_views_and_latest_revision AS (
+SELECT entity_id, page_views, page_latest AS latest_revision
+FROM wikidatawiki_p.page
+INNER JOIN s53311__wikidata_usage_and_views_p.entity_views
+ON page_title = entity_id;
