be00fd8dca8a3a76d45a977cc6f6014094bf9d28
hall1467 <hall1467@umn.edu>
2017-06-28 22:28:38 -0500

    Need Wikidata page_id field to join with revision table

--- sql_scripts/tool_labs/random_100000_items_table/random_100000_items_table_creation.sql
+++ sql_scripts/tool_labs/random_100000_items_table/random_100000_items_table_creation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE s53311__wikidata_usage_and_views_p.random_100000_items AS (
-	SELECT entity_id, page_views, latest_revision, page_namespace
+	SELECT wikidata_page_id, entity_id, page_views, latest_revision, page_namespace
 	FROM s53311__wikidata_usage_and_views_p.entity_views_and_latest_revision
 	WHERE page_namespace = 0
 	ORDER BY RAND()
