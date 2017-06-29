0f16037496fea9a66d03aa008223a49da1df5952
hall1467 <hall1467@umn.edu>
2017-06-28 19:21:14 -0500

    Updated query

--- sql_scripts/tool_labs/random_100000_pages_table/random_100000_pages_table_creation.sql
+++ sql_scripts/tool_labs/random_100000_pages_table/random_100000_pages_table_creation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE s53311__wikidata_usage_and_views_p.random_100000_pages_table_creation AS (
-	SELECT page_id, page_title, page_namespace
+	SELECT page_title, page_namespace
 	FROM wikidatawiki_p.page 
 	ORDER BY RAND()
 	LIMIT 100000
