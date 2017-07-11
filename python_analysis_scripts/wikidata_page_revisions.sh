f55312fcad9d3b7fa197e6d3a1708b26678415dc
hall1467 <hall1467@umn.edu>
2017-07-11 14:41:00 -0500

    fixed

--- python_analysis_scripts/wikidata_page_revisions.sh
+++ python_analysis_scripts/wikidata_page_revisions.sh
@@ -1,1 +1,1 @@
-python wikidata_page_revision.py --input=../../../wbc_entity_usage/wikidata_page_revisions/wikidatawiki-20170501-stub-meta-history.xml.gz revisions-output=../../../../wbc_entity_usage/usage_results/wikidata_page_revisions/wikidata_page_revisions_20170501.tsv
+python wikidata_page_revisions.py --input=../../../wbc_entity_usage/wikidata_page_revisions/wikidatawiki-20170501-stub-meta-history.xml.gz revisions-output=../../../../wbc_entity_usage/usage_results/wikidata_page_revisions/wikidata_page_revisions_20170501.tsv
