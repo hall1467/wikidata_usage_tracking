fc354bf00f81393206710c7549fc1889da79b145
hall1467 <hall1467@umn.edu>
2017-07-14 12:04:45 -0500

    renamed field

--- sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE wikidata_page_revisions(
-	page_title  VARCHAR(255),
-	revision_id BIGINT,
-	user        VARCHAR(265),
-	comment     BIGINT
+	page_title    VARCHAR(255),
+	revision_id   BIGINT,
+	revision_user VARCHAR(265),
+	comment       BIGINT
 );
