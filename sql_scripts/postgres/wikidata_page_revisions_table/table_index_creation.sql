4917bceb7f35e96fa8561cf508d9d8ded28a0f87
hall1467 <hall1467@umn.edu>
2017-07-18 09:35:49 -0500

    adjusted column names

--- sql_scripts/postgres/wikidata_page_revisions_table/table_index_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/table_index_creation.sql
@@ -1,4 +1,4 @@
 CREATE INDEX page_title_index ON wikidata_page_revisions(page_title);
 CREATE INDEX revision_id_index ON wikidata_page_revisions(revision_id);
-CREATE INDEX user_index ON wikidata_page_revisions(user);
+CREATE INDEX user_index ON wikidata_page_revisions(revision_user);
 CREATE INDEX comment_index ON wikidata_page_revisions(comment);
