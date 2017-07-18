6f11ea75d360865efaa6c32927c174a6d5f17e99
hall1467 <hall1467@umn.edu>
2017-07-17 22:11:12 -0500

    Updated size

--- sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
@@ -2,5 +2,5 @@ CREATE TABLE wikidata_page_revisions(
 	page_title    VARCHAR(255),
 	revision_id   BIGINT,
 	revision_user VARCHAR(265),
-	comment       VARCHAR(255)
+	comment       VARCHAR(355)
 );
