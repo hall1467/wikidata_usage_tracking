61f75b55de471743fd3f3fcb19a79e56c7f4fc5b
hall1467 <hall1467@umn.edu>
2017-07-19 14:41:12 -0500

    fixed syntax error(s)

--- sql_scripts/postgres/wikidata_page_revisions_table/wikidata_page_revisions_bot_info_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/wikidata_page_revisions_bot_info_table/table_creation.sql
@@ -1,6 +1,7 @@
-CREATE TABLE wikidata_page_revisions_bot_info_table AS(
+CREATE TABLE wikidata_page_revisions_bot_info AS (
 	SELECT wikidata_page_revisions.*, user_id AS bot_user_id
 	FROM wikidata_page_revisions
 	LEFT JOIN
 	wikidata_bots
+	ON user_id = revision_user
 );
