46fb4efcfa80ce695934e844cc841b8ff12ec9e6
hall1467 <hall1467@umn.edu>
2017-07-22 13:01:46 -0500

    changed extension

--- sql_scripts/postgres/monthly_item_quality_table/table_import.sql
+++ sql_scripts/postgres/monthly_item_quality_table/table_import.sql
@@ -1,1 +1,1 @@
-\COPY monthly_item_quality FROM '../../../../../wbc_entity_usage/monthly_item_quality/wikidatawiki-20170701.monthly_scores.tsv.bz2';
+\COPY monthly_item_quality FROM '../../../../../wbc_entity_usage/monthly_item_quality/wikidatawiki-20170701.monthly_scores.tsv';
