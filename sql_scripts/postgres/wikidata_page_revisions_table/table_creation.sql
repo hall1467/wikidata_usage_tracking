ec5bf423757470216c7058967e371fa71ef83555
hall1467 <hall1467@umn.edu>
2017-07-23 17:20:12 -0500

    Added namespace to schema

--- sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/table_creation.sql
@@ -2,5 +2,6 @@ CREATE TABLE wikidata_page_revisions(
 	page_title    VARCHAR(255),
 	revision_id   BIGINT,
 	revision_user VARCHAR(265),
-	comment       VARCHAR(355)
+	comment       VARCHAR(355),
+	namespace     BIGINT
 );
