21d898aee938f53004f313ba9bad716e8934e970
hall1467 <hall1467@umn.edu>
2017-07-24 16:52:26 -0500

    actually fixed bug now

--- sql_scripts/postgres/wikidata_page_revisions_table/entity_views_and_aggregated_revisions_table/queries/entity_views_and_aggregated_revisions_and_quality_scoring.sql
+++ sql_scripts/postgres/wikidata_page_revisions_table/entity_views_and_aggregated_revisions_table/queries/entity_views_and_aggregated_revisions_and_quality_scoring.sql
@@ -1,10 +1,1 @@
-\copy () TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/entity_views_and_aggregated_revisions/entity_views_and_aggregated_revisions_and_quality_scoring.tsv';
-
-SELECT aggregations.entity_id, aggregations.rev_id, aggregations.number_of_revisions, aggregations.page_views, predictions.prediction 
-FROM entity_views_and_aggregated_revisions AS aggregations 
-INNER JOIN 
-(
-	SELECT DISTINCT title, rev_id, prediction 
-	FROM monthly_item_quality 
-)   AS predictions 
-ON predictions.rev_id = aggregations.rev_id
+\copy (SELECT aggregations.entity_id, aggregations.rev_id, aggregations.number_of_revisions, aggregations.page_views, predictions.prediction FROM entity_views_and_aggregated_revisions AS aggregations INNER JOIN (SELECT DISTINCT title, rev_id, prediction FROM monthly_item_quality )   AS predictions ON predictions.rev_id = aggregations.rev_id) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/entity_views_and_aggregated_revisions/entity_views_and_aggregated_revisions_and_quality_scoring.tsv';
