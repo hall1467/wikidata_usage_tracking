3c0ff215d5c1157f863f1e723d0c2d216e6ede07
hall1467 <hall1467@umn.edu>
2017-07-27 13:04:58 -0500

    fixed bug

--- sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_most_viewed_entities_table.sql
+++ sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_most_viewed_entities_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE outliers.fifty_most_viewed_entities AS (
-	SELECT proj_aspect_entity.project, proj_aspect_entity.entity_id, proj_aspect_entity.aspect, proj_aspect_entity.page_id, proj_aspect_entity.page_id, total_entity_page_views 
+	SELECT proj_aspect_entity.project, proj_aspect_entity.entity_id, proj_aspect_entity.aspect, proj_aspect_entity.page_id, total_entity_page_views 
 	FROM 
 		(
 		  SELECT entity_id, SUM(page_views) AS total_entity_page_views 
