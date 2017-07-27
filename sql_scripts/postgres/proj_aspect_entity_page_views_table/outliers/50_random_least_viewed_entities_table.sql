5f6977d7be32144a9338c85c88eb3676b0a51bad
hall1467 <hall1467@umn.edu>
2017-07-27 13:16:27 -0500

    random 50 least valuable table and query

---
+++ sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_random_least_viewed_entities_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE outliers.50_random_least_viewed_entities AS (
+	SELECT * 
+	FROM (
+		SELECT entity_id, SUM(page_views) AS total_entity_page_views 
+		FROM (
+			SELECT DISTINCT project, entity_id, page_id, page_views 
+		  	FROM proj_aspect_entity_page_views
+		  	) AS entity_project_page_views 
+		GROUP BY entity_id 
+		HAVING SUM(page_views) = 0 ORDER BY RANDOM() limit 50
+		) AS entity_page_views 
+	INNER JOIN proj_aspect_entity_page_views 
+	ON entity_page_views.entity_id = proj_aspect_entity_page_views.entity_id
+)
