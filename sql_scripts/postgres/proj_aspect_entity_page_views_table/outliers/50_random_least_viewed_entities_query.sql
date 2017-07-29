1ccb5884923c90b70d89c0c6f38726229aaf9832
hall1467 <hall1467@umn.edu>
2017-07-29 15:41:19 -0500

    Adjusted tsv file order

--- sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_random_least_viewed_entities_query.sql
+++ sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_random_least_viewed_entities_query.sql
@@ -1,1 +1,1 @@
-\copy (SELECT entity_id, project, page_id, aspect, total_entity_page_views FROM outliers.fifty_random_least_viewed_entities) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/outliers/50_random_least_viewed_entities.tsv';
+\copy (SELECT entity_id, project, page_id, aspect, page_views FROM outliers.fifty_random_least_viewed_entities) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/outliers/50_random_least_viewed_entities.tsv';
