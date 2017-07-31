78a2d075df3850a574fe8d7eb8ddb1ea97adbb76
hall1467 <hall1467@umn.edu>
2017-07-30 20:32:56 -0500

    adding shell script to run script to get comment word counts

---
+++ python_analysis_scripts/revision_comment_word_extractor.sh
@@ -0,0 +1,1 @@
+ python revision_comment_word_extractor.py ../../../wbc_entity_usage/usage_results/wikidata_page_revisions/wikidata_page_revisions_20170501.tsv ../../../wbc_entity_usage/usage_results/wikidata_page_revisions/words_counts_for_wikidata_page_revisions_20170501.tsv --verbose
