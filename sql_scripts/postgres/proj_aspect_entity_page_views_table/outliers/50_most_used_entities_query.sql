d1fb046c6550610ad848a8f6df16f6d5b92ff9c2
hall1467 <hall1467@umn.edu>
2017-07-31 15:02:01 -0500

    Updated order

--- sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_most_used_entities_query.sql
+++ sql_scripts/postgres/proj_aspect_entity_page_views_table/outliers/50_most_used_entities_query.sql
@@ -1,1 +1,1 @@
-\copy (SELECT * from outliers.fifty_most_used_entities) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/outliers/50_most_used_entities.tsv'; 
+\copy (SELECT entity_id, aspect, project, page_id, total_entity_page_usages from outliers.fifty_most_used_entities) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/outliers/50_most_used_entities.tsv'; 
