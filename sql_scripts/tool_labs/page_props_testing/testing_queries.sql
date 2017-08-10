1a9feb83ccf900610975b0dc21c0d8d909839131
hall1467 <hall1467@umn.edu>
2017-08-10 11:25:58 -0500

    Updated

--- sql_scripts/tool_labs/page_props_testing/testing_queries.sql
+++ sql_scripts/tool_labs/page_props_testing/testing_queries.sql
@@ -10,14 +10,14 @@ CREATE TABLE s53311__wikidata_usage_and_views_p.2017_revisions AS
 (
 	select rev_page, rev_timestamp
 	from wikidatawiki_p.revision
-	where rev_timestamp >= 20170809000000 AND rev_timestamp < 20170809003000 and rev_comment like '/* wbsetsitelink-add:1|enwiki%'
+	where rev_timestamp >= 20170809000000 AND rev_timestamp < 20170810000000 and rev_comment like '/* wbsetsitelink-add:1|enwiki%'
 );
 
 CREATE TABLE s53311__wikidata_usage_and_views_p.2015_revisions AS
 (
 	select rev_page, rev_timestamp
 	from wikidatawiki_p.revision
-	where rev_timestamp >= 20150809000000 AND rev_timestamp < 20150809003000 and rev_comment like '/* wbsetsitelink-add:1|enwiki%'
+	where rev_timestamp >= 20150809000000 AND rev_timestamp < 20150810000000 and rev_comment like '/* wbsetsitelink-add:1|enwiki%'
 );
 
 
@@ -56,7 +56,7 @@ where page_namespace = 0 AND page_title NOT IN
 );
 
 select page_title
-from s53311__wikidata_usage_and_views_p.2017_revisions
+from s53311__wikidata_usage_and_views_p.2015_revisions
 INNER JOIN
 wikidatawiki_p.page
 ON page_id = rev_page
