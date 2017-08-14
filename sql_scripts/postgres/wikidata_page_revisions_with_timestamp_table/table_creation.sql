1f51aeba990c196213090bf619aeaaf85dd5e137
hall1467 <hall1467@umn.edu>
2017-08-14 18:59:16 -0500

    fixed bug

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_creation.sql
@@ -1,4 +1,4 @@
-CREATE TABLE wikidata_page_revisions(
+CREATE TABLE wikidata_page_revisions_with_timestamp(
 	page_title         VARCHAR(255),
 	revision_id        BIGINT,
 	revision_user      VARCHAR(265),
