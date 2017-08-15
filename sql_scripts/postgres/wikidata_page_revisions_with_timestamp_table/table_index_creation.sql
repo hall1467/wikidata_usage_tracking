e3d3024a3c71e25ad6d7e0b8dcdd550ae21af8c8
hall1467 <hall1467@umn.edu>
2017-08-14 21:18:29 -0500

    fixed bug

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_index_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_index_creation.sql
@@ -1,5 +1,5 @@
 CREATE INDEX revision_timestamp_index ON wikidata_page_revisions_with_timestamp(revision_timestamp);
-CREATE INDEX page_title_index ON wikidata_page_revisions_with_timestamp(page_title);
-CREATE INDEX revision_id_index ON wikidata_page_revisions_with_timestamp(revision_id);
-CREATE INDEX user_index ON wikidata_page_revisions_with_timestamp(revision_user);
-CREATE INDEX comment_index ON wikidata_page_revisions_with_timestamp(comment);
+CREATE INDEX with_timestamp_table_page_title_index ON wikidata_page_revisions_with_timestamp(page_title);
+CREATE INDEX with_timestamp_table_revision_id_index ON wikidata_page_revisions_with_timestamp(revision_id);
+CREATE INDEX with_timestamp_table_user_index ON wikidata_page_revisions_with_timestamp(revision_user);
+CREATE INDEX with_timestamp_table_comment_index ON wikidata_page_revisions_with_timestamp(comment);
