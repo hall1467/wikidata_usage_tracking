359a68264c4c533e0dd791daeff89c4d761b942c
hall1467 <hall1467@umn.edu>
2017-08-15 13:26:11 -0500

    Removed erroneous print statement

--- python_analysis_scripts/entity_usage_on_corresponding_articles/extract_page_information.sh
+++ python_analysis_scripts/entity_usage_on_corresponding_articles/extract_page_information.sh
@@ -1,2 +1,2 @@
 # The makefile in this directory works just fine. Used this to run extract_page_information seperately.
-python extract_page_information.py ../../../../wbc_entity_usage/page_storage/* > ../../../../wbc_entity_usage/usage_results/entity_usage_on_corresponding_articles/pages.json > & ../../../../wbc_entity_usage/usage_results/entity_usage_on_corresponding_articles/extract_page_information_stderr_log.txt
+python extract_page_information.py ../../../../wbc_entity_usage/page_storage/* > ../../../../wbc_entity_usage/usage_results/entity_usage_on_corresponding_articles/pages.json
