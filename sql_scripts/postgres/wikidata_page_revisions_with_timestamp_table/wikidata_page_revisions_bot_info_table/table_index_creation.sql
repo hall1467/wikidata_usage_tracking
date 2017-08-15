65820e6d3f5bddf0683266d970090fbc016c1c7a
hall1467 <hall1467@umn.edu>
2017-08-15 15:37:26 -0500

    right version

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/table_index_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/table_index_creation.sql
@@ -1,4 +1,5 @@
-CREATE INDEX page_title_index ON wikidata_page_revisions(page_title);
-CREATE INDEX revision_id_index ON wikidata_page_revisions(revision_id);
-CREATE INDEX user_index ON wikidata_page_revisions(revision_user);
-CREATE INDEX comment_index ON wikidata_page_revisions(comment);
+CREATE INDEX revision_and_bot_info_timestamp_index ON wikidata_page_revisions_with_timestamp(revision_timestamp);
+CREATE INDEX revision_and_bot_info_page_title_index ON wikidata_page_revisions_with_timestamp(page_title);
+CREATE INDEX revision_and_bot_info_revision_id_index ON wikidata_page_revisions_with_timestamp(revision_id);
+CREATE INDEX wevision_and_bot_info_user_index ON wikidata_page_revisions_with_timestamp(revision_user);
+CREATE INDEX revision_and_bot_info_comment_index ON wikidata_page_revisions_with_timestamp(comment);
