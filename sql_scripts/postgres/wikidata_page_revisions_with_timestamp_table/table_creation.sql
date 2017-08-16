0fa21d8e1043ea935c4dfca024ec4861016f19fb
hall1467 <hall1467@umn.edu>
2017-08-15 21:56:32 -0500

    table_creation.sql

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/table_creation.sql
@@ -4,5 +4,7 @@ CREATE TABLE wikidata_page_revisions_with_timestamp(
 	revision_user      VARCHAR(265),
 	comment            VARCHAR(355),
 	namespace          BIGINT,
-	revision_timestamp BIGINT
+	revision_timestamp BIGINT,
+	year               BIGINT,
+	month              BIGINT
 );
