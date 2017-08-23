28d3401bc8f650b9bc8963d4222a301edc9ae90c
hall1467 <hall1467@umn.edu>
2017-08-23 17:50:09 -0500

    Fixed bug

--- sql_scripts/postgres/proj_aspect_entity_page_views_table/usage_sampling/random_x_usages.sql
+++ sql_scripts/postgres/proj_aspect_entity_page_views_table/usage_sampling/random_x_usages.sql
@@ -1,1 +1,1 @@
-\copy (SELECT entity_id, aspect, project, page_id, page_views from proj_aspect_entity_page_views where aspect = 'X' order by random() limit 50) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/usage_sampling/random_non_x_non_s_usages.tsv'; 
+\copy (SELECT entity_id, aspect, project, page_id, page_views from proj_aspect_entity_page_views where aspect = 'X' order by random() limit 50) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/proj_aspect_entity_page_views/usage_sampling/random_x_usages.tsv'; 
