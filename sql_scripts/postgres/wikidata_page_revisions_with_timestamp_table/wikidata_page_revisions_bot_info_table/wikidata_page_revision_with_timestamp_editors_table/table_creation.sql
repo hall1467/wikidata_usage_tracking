f3c1f1a6dd1a2926c4f99190d44fde9562fc3700
hall1467 <hall1467@umn.edu>
2017-08-23 23:00:29 -0500

    Updated semi-automated query

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revision_with_timestamp_editors_table/table_creation.sql
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revision_with_timestamp_editors_table/table_creation.sql
@@ -1,4 +1,4 @@
-CREATE TABLE wikidata_page_revision_editors AS(
+CREATE TABLE wikidata_page_revision_with_timestamp_editors AS(
 	SELECT page_title, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits
 	FROM 
 	(
@@ -9,15 +9,29 @@ CREATE TABLE wikidata_page_revision_editors AS(
 			FROM 
 			(
 				SELECT page_title AS bot_edit_title, count(*) as bot_edits
-				FROM wikidata_page_revisions_bot_info
+				FROM wikidata_page_revisions_with_timestamp_bot_info
 				WHERE bot_user_id IS NOT NULL
 				GROUP BY page_title
 			) AS bot_edits_query
 			FULL OUTER JOIN
 			(
 				SELECT page_title AS non_bot_edit_title, count(*) as non_bot_edits
-				FROM wikidata_page_revisions_bot_info
-				WHERE bot_user_id IS NULL AND revision_user NOT LIKE '%.%' AND NOT (comment LIKE '%quickstatements%' OR comment LIKE '%petscan%' OR comment LIKE '%autolist2%')
+				FROM ikidata_page_revisions_with_timestamp_bot_info
+				WHERE bot_user_id IS NULL AND revision_user NOT LIKE '%.%' AND NOT (comment LIKE '%quickstatements%' OR 
+																					comment LIKE '%petscan%' OR 
+																					comment LIKE '%autolist2%' OR
+																					comment LIKE '%autoedit%' OR
+																					comment LIKE '%nameguzzler%' OR 
+																					comment LIKE '%labellister%' OR
+																					comment LIKE '%#itemcreator%' OR 
+																					comment LIKE '%#dragrefjs%' OR 
+																					comment LIKE '%[[useryms/lc|lcjs]]%' OR 
+																					comment LIKE '%#wikidatagame%' OR 
+																					comment LIKE '%[[wikidataprimary%' OR
+																					comment LIKE '%#mix\'n\'match%' OR 
+																					comment LIKE '%mix\'n\'match%' OR
+																					comment LIKE '%#distributedgame%' OR  
+																					comment LIKE '%[[userjitrixis/nameguzzlerjs|nameguzzler]]%')
 				GROUP BY page_title
 			) AS non_bot_edits_query
 			ON bot_edit_title = non_bot_edit_title
@@ -25,8 +39,22 @@ CREATE TABLE wikidata_page_revision_editors AS(
 		FULL OUTER JOIN
 		(
 			SELECT page_title AS anon_edit_title, count(*) as anon_edits
-			FROM wikidata_page_revisions_bot_info
-			WHERE bot_user_id IS NULL AND revision_user LIKE '%.%' AND NOT (comment LIKE '%quickstatements%' OR comment LIKE '%petscan%' OR comment LIKE '%autolist2%')
+			FROM ikidata_page_revisions_with_timestamp_bot_info
+			WHERE bot_user_id IS NULL AND revision_user LIKE '%.%' AND NOT (comment LIKE '%quickstatements%' OR 
+																					comment LIKE '%petscan%' OR 
+																					comment LIKE '%autolist2%' OR
+																					comment LIKE '%autoedit%' OR
+																					comment LIKE '%nameguzzler%' OR 
+																					comment LIKE '%labellister%' OR
+																					comment LIKE '%#itemcreator%' OR 
+																					comment LIKE '%#dragrefjs%' OR 
+																					comment LIKE '%[[useryms/lc|lcjs]]%' OR 
+																					comment LIKE '%#wikidatagame%' OR 
+																					comment LIKE '%[[wikidataprimary%' OR
+																					comment LIKE '%#mix\'n\'match%' OR 
+																					comment LIKE '%mix\'n\'match%' OR
+																					comment LIKE '%#distributedgame%' OR  
+																					comment LIKE '%[[userjitrixis/nameguzzlerjs|nameguzzler]]%')
 			GROUP BY page_title
 		) AS anons
 		ON bot_edit_title = anon_edit_title
@@ -37,14 +65,14 @@ CREATE TABLE wikidata_page_revision_editors AS(
 		FROM
 		(
 			SELECT page_title AS semi_automated_title, count(*) as semi_automated_edits
-			FROM wikidata_page_revisions_bot_info
+			FROM ikidata_page_revisions_with_timestamp_bot_info
 			WHERE bot_user_id IS NULL AND (comment LIKE '%quickstatements%' OR comment LIKE '%petscan%' OR comment LIKE '%autolist2%')
 			GROUP BY page_title
 		) AS semi_automated_revisions
 		FULL OUTER JOIN
 		(
 			SELECT page_title, count(*) as all_edits
-			FROM wikidata_page_revisions_bot_info
+			FROM ikidata_page_revisions_with_timestamp_bot_info
 			GROUP BY page_title
 		) AS all_revisions
 		ON page_title = semi_automated_title
