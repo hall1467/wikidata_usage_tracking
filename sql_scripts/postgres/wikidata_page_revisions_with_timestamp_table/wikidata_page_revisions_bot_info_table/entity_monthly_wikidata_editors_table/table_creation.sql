2183dd148e3065169cebf149f440346943742d34
hall1467 <hall1467@umn.edu>
2017-08-28 17:06:05 -0500

    Adding new table for misalingment versus alignment analyses

---
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/entity_monthly_wikidata_editors_table/table_creation.sql
@@ -0,0 +1,95 @@
+CREATE TABLE entity_monthly_wikidata_editors AS(
+	SELECT page_title, year, month, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits
+	FROM 
+	(
+		SELECT *
+		FROM 
+		(
+			SELECT *
+			FROM 
+			(
+				SELECT page_title AS bot_edit_page_title, year AS bot_edit_year, month AS bot_edit_month, count(*) as bot_edits
+				FROM wikidata_page_revisions_with_timestamp_bot_info
+				WHERE bot_user_id IS NOT NULL
+				GROUP BY page_title, year, month
+			) AS bot_edits_query
+			FULL OUTER JOIN
+			(
+				SELECT page_title AS non_bot_edit_page_title, year AS non_bot_edit_year, month AS non_bot_edit_month, count(*) as non_bot_edits
+				FROM wikidata_page_revisions_with_timestamp_bot_info
+				WHERE bot_user_id IS NULL AND revision_user NOT LIKE '%.%' AND NOT (lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%quickstatements%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%petscan%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autolist2%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autoedit%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%nameguzzler%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%labellister%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#itemcreator%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#dragrefjs%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[useryms/lc|lcjs]]%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#wikidatagame%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[wikidataprimary%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#mix''n''match%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%mix''n''match%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#distributedgame%' OR  
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[userjitrixis/nameguzzlerjs|nameguzzler]]%')
+				GROUP BY page_title, year, month
+			) AS non_bot_edits_query
+			ON bot_edit_page_title = non_bot_page_title AND bot_edit_year = non_bot_edit_year AND bot_edit_month = non_bot_edit_month
+		) AS bots_and_non_bots
+		FULL OUTER JOIN
+		(
+			SELECT page_title AS anon_edit_page_title, year AS anon_edit_year, month AS anon_edit_month, count(*) as anon_edits
+			FROM wikidata_page_revisions_with_timestamp_bot_info
+			WHERE bot_user_id IS NULL AND revision_user LIKE '%.%' AND NOT (lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%quickstatements%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%petscan%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autolist2%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autoedit%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%nameguzzler%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%labellister%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#itemcreator%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#dragrefjs%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[useryms/lc|lcjs]]%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#wikidatagame%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[wikidataprimary%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#mix''n''match%' OR 
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%mix''n''match%' OR
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#distributedgame%' OR  
+																					lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[userjitrixis/nameguzzlerjs|nameguzzler]]%')
+			GROUP BY page_title, year, month
+		) AS anons
+		ON bot_edit_page_title = anon_edit_page_title AND bot_edit_year = anon_edit_year AND bot_edit_month = anon_edit_month
+	) AS bots_and_non_bots_and_anons
+	FULL OUTER JOIN
+	(
+		SELECT *
+		FROM
+		(
+			SELECT page_title AS semi_automated_edit_page_title, year AS semi_automated_edit_year, month AS semi_automated_edit_month, count(*) as semi_automated_edits
+			FROM wikidata_page_revisions_with_timestamp_bot_info
+			WHERE bot_user_id IS NULL AND (lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%quickstatements%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%petscan%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autolist2%' OR
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%autoedit%' OR
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%nameguzzler%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%labellister%' OR
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#itemcreator%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#dragrefjs%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[useryms/lc|lcjs]]%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#wikidatagame%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[wikidataprimary%' OR
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#mix''n''match%' OR 
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%mix''n''match%' OR
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%#distributedgame%' OR  
+										   lower(regexp_replace(comment, '\.|,|\(|\)|-|:','','g')) LIKE '%[[userjitrixis/nameguzzlerjs|nameguzzler]]%')
+			GROUP BY page_title, year, month
+		) AS semi_automated_revisions
+		FULL OUTER JOIN
+		(
+			SELECT page_title, year, month, count(*) as all_edits
+			FROM wikidata_page_revisions_with_timestamp_bot_info
+			GROUP BY page_title, year, month
+		) AS all_revisions
+		ON page_title = semi_automated_edit_page_title AND year = semi_automated_edit_year AND month = semi_automated_edit_month
+	) AS semi_automated_and_all
+	ON page_title = bot_edit_page_title AND year = bot_edit_year AND month = bot_edit_month
+);
