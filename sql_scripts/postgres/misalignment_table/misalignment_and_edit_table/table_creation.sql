baf3c4df9c9a14d71bd0d3d5aac11c70cc1862b8
hall1467 <hall1467@umn.edu>
2017-09-07 17:54:02 -0500

    Updated table

--- sql_scripts/postgres/misalignment_table/misalignment_and_edit_table/table_creation.sql
+++ sql_scripts/postgres/misalignment_table/misalignment_and_edit_table/table_creation.sql
@@ -1,8 +1,8 @@
 CREATE TABLE misalignment_and_edit AS (
 SELECT misalignment.entity_id, misalignment.year, misalignment.month, quality_class, views_class, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits 
-FROM entity_monthly_wikidata_editors 
+FROM entity_previous_month_aggregated_edits 
 INNER JOIN misalignment 
-ON misalignment.entity_id = entity_monthly_wikidata_editors.page_title 
-	AND misalignment.year = entity_monthly_wikidata_editors.year 
-	AND misalignment.month = entity_monthly_wikidata_editors.month
+ON misalignment.entity_id = entity_previous_month_aggregated_edits.page_title 
+	AND misalignment.year = entity_previous_month_aggregated_edits.year 
+	AND misalignment.month = entity_previous_month_aggregated_edits.month
 );
