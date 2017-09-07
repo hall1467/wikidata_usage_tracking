94861efd2b8b6285b91d04601c6ee78f61f1e887
hall1467 <hall1467@umn.edu>
2017-09-07 17:56:23 -0500

    Updated table

---
+++ sql_scripts/postgres/misalignment_table/misalignment_and_edits_table/table_creation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE misalignment_and_edits AS (
+SELECT misalignment.entity_id, misalignment.year, misalignment.month, quality_class, views_class, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits 
+FROM entity_previous_month_aggregated_edits 
+INNER JOIN misalignment 
+ON misalignment.entity_id = entity_previous_month_aggregated_edits.page_title 
+	AND misalignment.year = entity_previous_month_aggregated_edits.year 
+	AND misalignment.month = entity_previous_month_aggregated_edits.month
+);
