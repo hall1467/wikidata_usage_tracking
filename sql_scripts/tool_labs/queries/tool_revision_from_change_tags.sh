040c2e6aab80fb614cc8690dc812942da54d01f2
hall1467 <hall1467@umn.edu>
2017-10-10 17:10:57 -0500

    removing null revision ids

--- sql_scripts/tool_labs/queries/tool_revision_from_change_tags.sh
+++ sql_scripts/tool_labs/queries/tool_revision_from_change_tags.sh
@@ -1,1 +1,1 @@
-sql wikidatawiki_p "SELECT ct_rev_id FROM wikidatawiki_p.change_tag WHERE ct_tag LIKE 'OAuth CID:%';" > ../../../../tool_revision_from_change_tag.tsv
+sql wikidatawiki_p "SELECT ct_rev_id FROM wikidatawiki_p.change_tag WHERE ct_tag LIKE 'OAuth CID:%' AND ct_rev_id IS NOT NULL;" > ../../../../tool_revision_from_change_tag.tsv
