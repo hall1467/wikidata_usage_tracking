7ae405a7f8d2f74a38410dfe968b2bbee064e7b6
hall1467 <hall1467@umn.edu>
2017-10-18 19:49:47 -0500

    removing query

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/monthly_wikidata_editors_table/queries/all_months_sorted.sql
+++
@@ -1,1 +0,0 @@
-\copy (select year, month, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits from monthly_wikidata_editors order by year, month) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/monthly_wikidata_editors/all_months_sorted.csv' WITH HEADER CSV; 
