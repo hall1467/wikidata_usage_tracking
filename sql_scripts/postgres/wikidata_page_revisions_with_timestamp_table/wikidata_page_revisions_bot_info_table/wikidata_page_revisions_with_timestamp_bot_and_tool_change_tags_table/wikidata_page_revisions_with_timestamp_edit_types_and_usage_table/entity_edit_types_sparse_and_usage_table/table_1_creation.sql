fe9f6c0ccad6cee3f348cea724dccb9e3bb4cf08
hall1467 <hall1467@umn.edu>
2017-10-20 17:34:18 -0500

    Adding seperate tables which will then be put together for the full table

---
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/entity_edit_types_sparse_and_usage_table/table_1_creation.sql
@@ -0,0 +1,25 @@
+CREATE TABLE entity_edit_types_sparse_and_usage_sub_table_1 AS (
+	SELECT * 
+	FROM crosstab(
+		          'SELECT year_month_page_title, page_views, number_of_revisions, page_title, year, month, namespace, edit_type, count(*) 
+		           FROM wikidata_page_revisions_with_timestamp_edit_types_and_usage
+		           WHERE page_title < 'Q3'
+		           GROUP BY year_month_page_title, page_views, number_of_revisions, page_title, year, month, namespace, edit_type 
+		           ORDER BY year_month_page_title',
+		           'SELECT DISTINCT edit_type 
+		           FROM wikidata_page_revisions_with_timestamp_edit_types_and_usage 
+		           ORDER BY edit_type'
+		          ) AS (
+		                year_month_page_title TEXT, 
+		                page_views NUMERIC,
+		                number_of_revisions BIGINT,
+		                page_title VARCHAR(255),
+		                year BIGINT,
+		                month BIGINT,
+		                namespace BIGINT,
+		                anon_edits BIGINT, 
+		                bot_edits BIGINT, 
+		                human_edits BIGINT, 
+		                semi_automated_edits BIGINT
+		               )
+);
