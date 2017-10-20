cf276f3d91ad018a2c3b6538857d3e9cfe575d02
hall1467 <hall1467@umn.edu>
2017-10-20 14:12:34 -0500

    fixed typo

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
@@ -1,4 +1,4 @@
 psql wikidata_entities < anon_edits_for_used_entities.sql
-psql wikidata_entities < edit_types_sum.sql
+psql wikidata_entities < edit_type_sums.sql
 psql wikidata_entities < random_revisions.sql
 psql wikidata_entities < semi_automated_revision_word_counts.sql
