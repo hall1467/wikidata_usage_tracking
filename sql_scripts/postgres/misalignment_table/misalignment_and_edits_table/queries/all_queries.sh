4c94e2839c6c0c531fb1f12c4a6c70dd39db7988
hall1467 <hall1467@umn.edu>
2017-10-24 09:09:17 -0500

    Updated queries and created script to run them

---
+++ sql_scripts/postgres/misalignment_table/misalignment_and_edits_table/queries/all_queries.sh
@@ -0,0 +1,5 @@
+psql wikidata_entities < monthly_edit_types.sql
+psql wikidata_entities < monthly_misalignment_and_edits_ordered_by_entity_year_month.sql
+psql wikidata_entities < edit_proportions_by_entity_views_5_17.sql
+psql wikidata_entities < edit_proportions_by_quality_and_view_class_5_17.sql
+psql wikidata_entities < edit_proportions_by_quality_class_5_17.sql
--- sql_scripts/postgres/misalignment_table/misalignment_and_edits_table/queries/monthly_misalignment_and_edits.sql
+++
@@ -1,1 +0,0 @@
-\copy (select entity_id, year, month, quality_class, views_class, bot_edits, semi_automated_edits, non_bot_edits, anon_edits, all_edits from misalignment_and_edits order by year, month) TO '../../../../../../../wbc_entity_usage/usage_results/sql_queries/entity_monthly_wikidata_editors/monthly_misalignment_and_edits.tsv'; 
