fb22dd8cc099b402c8e4499a21da2cfe8edb86ae
hall1467 <hall1467@umn.edu>
2017-10-31 10:55:45 -0500

    adding new query for all edits

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
@@ -1,3 +1,4 @@
+psql wikidata_entities < all_edits_for_user_entities.sql
 source anon_edits_for_used_entities_query_and_post_processing.sh
 source registered_users_edits_for_used_entities_query_and_post_processing.sh
 psql wikidata_entities < used_entity_revision_comments.sql
