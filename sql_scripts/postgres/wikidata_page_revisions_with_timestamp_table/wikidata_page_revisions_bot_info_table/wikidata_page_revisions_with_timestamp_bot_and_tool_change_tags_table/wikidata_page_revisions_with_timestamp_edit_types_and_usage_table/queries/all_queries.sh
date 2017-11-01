98deb1508e6408108c8736a3268d9b8c12e023ed
hall1467 <hall1467@umn.edu>
2017-11-01 14:07:51 -0500

    fixed typo

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
@@ -1,6 +1,6 @@
 source anon_edits_for_used_entities_query_and_post_processing.sh
 source registered_users_edits_for_used_entities_query_and_post_processing.sh
-psql wikidata_entities < all_edits_for_user_entities.sql
+psql wikidata_entities < all_edits_for_used_entities.sql
 psql wikidata_entities < used_entity_revision_comments.sql
 psql wikidata_entities < edit_type_sums.sql
 psql wikidata_entities < random_revisions.sql
