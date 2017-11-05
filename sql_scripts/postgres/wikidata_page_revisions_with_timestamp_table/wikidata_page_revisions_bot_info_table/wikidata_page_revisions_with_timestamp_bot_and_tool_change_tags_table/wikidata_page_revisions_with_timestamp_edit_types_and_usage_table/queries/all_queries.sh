db50483c9008d40b49aaf627d28fef30e8a2db66
hall1467 <hall1467@umn.edu>
2017-11-05 13:59:55 -0600

    large updates

--- sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/all_queries.sh
@@ -1,7 +1,8 @@
+# Have been running on the virtual environment on flagon here: /export/scratch2/wmf/scripts/
+source all_edits_for_used_entities_query_and_post_processing.sh
 source anon_edits_for_used_entities_query_and_post_processing.sh
 source registered_users_edits_for_used_entities_query_and_post_processing.sh
 psql wikidata_entities < all_edits_for_used_entities.sql
-psql wikidata_entities < used_entity_revision_comments.sql
 psql wikidata_entities < edit_type_sums.sql
 psql wikidata_entities < random_revisions.sql
 psql wikidata_entities < semi_automated_revision_word_counts.sql
