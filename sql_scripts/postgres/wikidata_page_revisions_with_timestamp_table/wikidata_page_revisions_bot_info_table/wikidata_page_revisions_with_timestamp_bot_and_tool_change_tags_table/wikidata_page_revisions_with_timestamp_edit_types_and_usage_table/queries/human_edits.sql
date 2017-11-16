b5b3b47c05c296c28e5344806536d6d8314fb4e5
hall1467 <hall1467@umn.edu>
2017-11-16 13:31:41 -0600

    Added in new query

---
+++ sql_scripts/postgres/wikidata_page_revisions_with_timestamp_table/wikidata_page_revisions_bot_info_table/wikidata_page_revisions_with_timestamp_bot_and_tool_change_tags_table/wikidata_page_revisions_with_timestamp_edit_types_and_usage_table/queries/human_edits.sql
@@ -0,0 +1,1 @@
+\copy (select revision_user, revision_timestamp, revision_id from wikidata_page_revisions_with_timestamp_edit_types_and_usage where edit_type != 'human_edit' AND namespace = 0 order by revision_timestamp) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/wikidata_page_revisions_with_timestamp_edit_types_and_usage/human_edits.tsv';
