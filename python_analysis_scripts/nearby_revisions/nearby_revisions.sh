725ca06f38ec279b9a9c5816a1c204e057124ced
hall1467 <hall1467@umn.edu>
2017-11-22 09:05:54 -0600

    fixed bug

--- python_analysis_scripts/nearby_revisions/nearby_revisions.sh
+++ python_analysis_scripts/nearby_revisions/nearby_revisions.sh
@@ -1,11 +1,11 @@
 
-psql wikidata_entities -c "\copy (select revision_id, user, revision_timestamp, edit_type from wikidata_page_revisions_with_timestamp_edit_types_and_usage order by random() limit 100000) TO '/export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions.tsv';"
+psql wikidata_entities -c "\copy (select revision_id, revision_user, revision_timestamp, edit_type from wikidata_page_revisions_with_timestamp_edit_types_and_usage order by random() limit 100000) TO '/export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions.tsv';"
 
 echo "revision_id\tuser_id\ttimestamp\tedit_type" > /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions_with_header.tsv
 
 cat /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions.tsv >> /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions_with_header.tsv
 
-python /export/scratch2/wmf/scripts/wikidata_usage_tracking/python_analysis_scripts/nearby_revisions/nearby_revisions.py /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions_with_header.tsv nearby_revisions_for_100000_item_and_property_revisions.tsv --verbose > & /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions.tsv nearby_revisions_for_100000_item_and_property_revisions_error_log.tsv
+python /export/scratch2/wmf/scripts/wikidata_usage_tracking/python_analysis_scripts/nearby_revisions/nearby_revisions.py /export/scratch2/wmf/edit_analyses/nearby_revisions/100000_random_item_or_property_revisions_with_header.tsv /export/scratch2/wmf/edit_analyses/nearby_revisions/nearby_revisions_for_100000_item_and_property_revisions.tsv --verbose > & /export/scratch2/wmf/edit_analyses/nearby_revisions/nearby_revisions_for_100000_item_and_property_revisions_error_log.tsv
 
 mwsessions sessionize /export/scratch2/wmf/edit_analyses/nearby_revisions/nearby_revisions_for_100000_item_and_property_revisions.tsv --events=/export/scratch2/wmf/edit_analyses/nearby_revisions/nearby_revisions_for_100000_item_and_property_revisions_session_data.tsv --verbose > /export/scratch2/wmf/edit_analyses/nearby_revisions/nearby_revisions_for_100000_item_and_property_revisions_session_data.tsv
 # Need a script that will go back and remove sessions that do not contain one of the original 100000 item or property edits
