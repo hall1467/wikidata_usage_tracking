9b36257c10858d65f61467da35c24f4740963635
hall1467 <hall1467@umn.edu>
2017-11-22 22:20:47 -0600

    Different method

---
+++ python_analysis_scripts/edit_analyses/nearby_revisions.py
@@ -0,0 +1,147 @@
+"""
+Takes revision id, user id, timestamp, and edit type TSV (specifically ordered) and gives back nearby 
+revisions(using Mediawiki API). User can specify how "near" (in minutes).
+
+Usage:
+    nearby_revisions (-h|--help)
+    nearby_revisions <input> <output>
+                     [--debug]
+                     [--verbose]
+
+Options:
+    -h, --help  This help message is printed
+    <input>     Path to file to process.
+    <output>    Where output will be written
+    --debug     Print debug logging to stderr
+    --verbose   Print dots and stuff to stderr  
+"""
+
+
+import docopt
+import logging
+import operator
+import mwapi
+import sys
+import datetime
+import iso8601
+from collections import defaultdict
+import re
+import mysqltsv
+
+
+WIKIDATA_ITEM_RE = re.compile(r'^Q\d+')
+logger = logging.getLogger(__name__)
+
+
+def main(argv=None):
+    args = docopt.docopt(__doc__)
+    logging.basicConfig(
+        level=logging.INFO if not args['--debug'] else logging.DEBUG,
+        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
+    )
+
+    input_file = mysqltsv.Reader(open(args['<input>'],'rt'),
+        headers=True, types=[int, int, str, str])
+
+
+    output_file = mysqltsv.Writer(open(args['<output>'], "w"), headers=[
+    'timestamp', 'revision_id', 'user_id', 'namespace', 'edit_type'])
+
+    verbose = args['--verbose']
+
+    run(input_file, output_file, verbose)
+
+
+def run(input_file, output_file, verbose):
+
+    unique_revision_ids = defaultdict(lambda: defaultdict(dict))
+    session_for_nearby_revisions = mwapi.Session("http://wikidata.org")
+
+    for i, line in enumerate(input_file):
+
+        # Need to convert timestamp and then add and subtract from it
+        timestamp_year = int(line['timestamp'][0:4])
+        timestamp_month = int(line['timestamp'][4:6])
+        timestamp_day = int(line['timestamp'][6:8])
+        timestamp_hour = int(line['timestamp'][8:10])
+        timestamp_minute = int(line['timestamp'][10:12])
+        timestamp_second = int(line['timestamp'][12:14])
+
+
+        cast_timestamp = datetime.datetime(timestamp_year,
+                                           timestamp_month,
+                                           timestamp_day,
+                                           timestamp_hour,
+                                           timestamp_minute,
+                                           timestamp_second
+                                          )
+        
+        incremented_cast_timestamp =\
+            cast_timestamp + datetime.timedelta(minutes=61)
+        decremented_cast_timestamp =\
+            cast_timestamp - datetime.timedelta(minutes=61)
+
+     
+        # Range after
+        for revision_id, user_id, timestamp, namespace in request_function_and_timestamp_adjusting(cast_timestamp, incremented_cast_timestamp, line, session_for_nearby_revisions):
+            unique_revision_ids[timestamp][revision_id]["user_id"] = user_id
+            unique_revision_ids[timestamp][revision_id]["edit_type"] = line["edit_type"]
+            unique_revision_ids[timestamp][revision_id]["namespace"] = namespace
+
+        # Range before
+        for revision_id, user_id, timestamp, namespace in request_function_and_timestamp_adjusting(decremented_cast_timestamp, cast_timestamp, line, session_for_nearby_revisions):
+            unique_revision_ids[timestamp][revision_id]["user_id"] = user_id
+            unique_revision_ids[timestamp][revision_id]["edit_type"] = line["edit_type"]
+            unique_revision_ids[timestamp][revision_id]["namespace"] = namespace
+
+
+        if verbose:
+            sys.stderr.write("Revisions returned: {0}\n".format(i+1))  
+            sys.stderr.flush()
+
+
+    for timestamp in sorted(unique_revision_ids.keys()):
+        for revision_id in unique_revision_ids[timestamp]:
+            output_file.write(
+                [timestamp, 
+                 revision_id,
+                 unique_revision_ids[timestamp][revision_id]["user_id"],
+                 unique_revision_ids[timestamp][revision_id]["namespace"], 
+                 unique_revision_ids[timestamp][revision_id]["edit_type"]])
+
+
+
+
+
+
+
+def request_function_and_timestamp_adjusting(cast_timestamp, 
+    later_cast_timestamp, line, session_for_nearby_revisions):
+    for user_and_timestamp in session_for_nearby_revisions.get(action="query", format="json", list="usercontribs", uclimit="max", ucstart=cast_timestamp, 
+        ucend=later_cast_timestamp, ucdir="newer", ucprop=["timestamp","ids", "title"], ucuserids=[line["user_id"]], continuation=True):
+        for line in user_and_timestamp:
+            for nearby_revision in user_and_timestamp["query"]["usercontribs"]:
+
+                namespace = extract_revision_namespace(nearby_revision["title"])
+                nearby_revision_timestamp = iso8601.parse_date(nearby_revision["timestamp"])
+                
+                cast_nearby_revision_timestamp = str(nearby_revision_timestamp.year).zfill(4) + str(nearby_revision_timestamp.month).zfill(2) + str(nearby_revision_timestamp.day).zfill(2) + str(nearby_revision_timestamp.hour).zfill(2) + str(nearby_revision_timestamp.minute).zfill(2) + str(nearby_revision_timestamp.second).zfill(2)
+
+                yield nearby_revision["revid"], nearby_revision["userid"], cast_nearby_revision_timestamp, namespace
+
+
+def extract_revision_namespace(revision_title):
+    split_result = revision_title.split(":")
+    if len(split_result) > 1:
+        namespace = split_result[0]
+    elif WIKIDATA_ITEM_RE.match(split_result[0]):
+        namespace = "Main"
+    else:
+        raise RuntimeError("Title was not parsed correctly: {0}".format(revision_title))
+
+    return namespace
+
+
+
+main()
+
---
+++ python_analysis_scripts/edit_analyses/wikidata_revision_extraction.py
@@ -0,0 +1,103 @@
+"""
+Processes page revision dump data to return revisons for pages and the user who 
+created the revision.
+
+Usage:
+    wikidata_revision_extraction (-h|--help)
+    wikidata_revision_extraction <input>... --revisions-output=<location>
+                                       [--debug]
+                                       [--verbose]
+
+Options:
+    -h, --help                     This help message is printed
+    <input>                        Path to file(s) to process.
+    --revisions-output=<location>  Where revisions results
+                                   will be written
+    --debug                        Print debug logging to stderr
+    --verbose                      Print dots and stuff to stderr  
+"""
+
+
+import docopt
+import sys
+import logging
+import mwxml
+import mysqltsv
+
+logger = logging.getLogger(__name__)
+
+def main(argv=None):
+    args = docopt.docopt(__doc__)
+    logging.basicConfig(
+        level=logging.INFO if not args['--debug'] else logging.DEBUG,
+        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
+    )
+
+    input_files = args['<input>']
+
+    revisions_output_file =\
+        mysqltsv.Writer(open(args['--revisions-output'], "w"))
+
+    verbose = args['--verbose']
+
+    run(input_files, revisions_output_file, verbose)
+
+
+def run(input_files, revisions_output_file, verbose):
+
+    def process_pages(stub_file_dump_object, file_url):
+        for stub_file_page in stub_file_dump_object:
+            
+            if stub_file_page.namespace == 0 or stub_file_page.namespace == 120:
+
+                for stub_file_page_revision in stub_file_page:
+
+                    revision_comment = stub_file_page_revision.comment
+                    revision_user_id = "NULL"
+                    revision_user_text = "NULL"
+
+                    if revision_comment is None:
+                        revision_comment = "NULL"
+
+                    if stub_file_page_revision.user is None:       
+                        logger.warning(
+                            "No user. Fields will be NULL. Revision: {0}"
+                            .format(stub_file_page_revision))
+                        revision_user_id = "NULL"
+                        revision_user_text = "NULL"
+                    elif stub_file_page_revision.user.id is None:
+                        revision_user_id = "NULL"
+                        revision_user_text = stub_file_page_revision.user.text
+                    else:
+                        revision_user_id = stub_file_page_revision.user.id
+                        revision_user_text = stub_file_page_revision.user.text
+
+                        
+                    yield stub_file_page_revision.page.title,\
+                          stub_file_page_revision.id,\
+                          revision_user_id,\
+                          revision_user_text,\
+                          revision_comment,\
+                          stub_file_page.namespace,\
+                          stub_file_page_revision.timestamp
+
+    i = 0
+    revisions_output_file.write(["page_title", "revision_id", "user_id_or_ip", 
+        "comment", "namespace", "timestamp"])
+    for title, revision_id, user_id, user_text, comment, namespace, timestamp 
+        in mwxml.map(process_pages, input_files):
+        i += 1
+        revisions_output_file.write([title, revision_id, user_id, user_text, 
+            comment, namespace, timestamp])
+
+        if verbose and i % 10000 == 0:
+            sys.stderr.write("Revisions processed: {0}\n".format(i))  
+            sys.stderr.flush()
+
+    if verbose:
+        sys.stderr.write("Completed writing out result file\n")
+        sys.stderr.flush()
+
+
+main()
+
