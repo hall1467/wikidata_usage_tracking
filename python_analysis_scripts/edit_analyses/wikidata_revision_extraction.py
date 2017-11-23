6eff4ea6649c58b157e15a023b74f99cde8a70fa
hall1467 <hall1467@umn.edu>
2017-11-23 09:04:50 -0600

    Fixed bug

--- python_analysis_scripts/edit_analyses/wikidata_revision_extraction.py
+++ python_analysis_scripts/edit_analyses/wikidata_revision_extraction.py
@@ -73,7 +73,8 @@ def run(input_files, revisions_output_file, verbose):
 
 
                 timestamp =\
-                    datetime.datetime.utcfromtimestamp(stub_file_page_revision.timestamp)
+                    datetime.datetime.
+                        utcfromtimestamp(stub_file_page_revision.timestamp)
                     
                 cast_timestamp =\
                     str(timestamp.year).zfill(4) +\
