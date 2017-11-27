7d05fb6565167f9867eb3b1432795afab1214898
hall1467 <hall1467@umn.edu>
2017-11-27 15:50:58 -0600

    All bots

--- sql_scripts/postgres/wikidata_bots_table/queries/bot_user_id.sql
+++
@@ -1,1 +0,0 @@
-\copy (SELECT user_id FROM wikidata_bots) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/wikidata_bots_table/bot_user_ids.tsv'; 
---
+++ sql_scripts/postgres/wikidata_bots_table/queries/bot_user_ids.sql
@@ -0,0 +1,1 @@
+\copy (SELECT user_id FROM wikidata_bots) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/sql_queries/wikidata_bots/bot_user_ids.tsv'; 
