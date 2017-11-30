a14a7e2eb7ada1f368cc5d85e096e65123e58e3b
hall1467 <hall1467@umn.edu>
2017-11-30 12:26:00 -0600

    No longer running comleted scripte

--- python_analysis_scripts/edit_analyses/edit_analyses.sh
+++ python_analysis_scripts/edit_analyses/edit_analyses.sh
@@ -25,7 +25,7 @@
 
 #python /export/scratch2/wmf/scripts/wikidata_usage_tracking/python_analysis_scripts/edit_analyses/session_stats.py /export/scratch2/wmf/edit_analyses/revisions_from_100000_random_registered_human_and_bot_sessions_containing_item_or_property_edits_labelled.tsv /export/scratch2/wmf/edit_analyses/session_stats.txt --verbose --debug > & /export/scratch2/wmf/edit_analyses/session_stats_error_log.txt
 
-python /export/scratch2/wmf/scripts/wikidata_usage_tracking/python_analysis_scripts/edit_analyses/predictor_construction.py /export/scratch2/wmf/edit_analyses/revisions_from_100000_random_registered_human_and_bot_sessions_containing_item_or_property_edits_labelled.tsv /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data.tsv --verbose --debug > & /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data_error_log.tsv
+#python /export/scratch2/wmf/scripts/wikidata_usage_tracking/python_analysis_scripts/edit_analyses/predictor_construction.py /export/scratch2/wmf/edit_analyses/revisions_from_100000_random_registered_human_and_bot_sessions_containing_item_or_property_edits_labelled.tsv /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data.tsv --verbose --debug > & /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data_error_log.tsv
 
 cat /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data.tsv | grep -vP "NULL\tNULL" > /export/scratch2/wmf/edit_analyses/predictors_and_labelled_data_revisions_containing_sessions_with_greater_than_2_revisions.tsv
 
