ad500532f141657b6837874ac7524185ceb2d624
hall1467 <hall1467@umn.edu>
2017-12-01 14:06:45 -0600

    fixed bug

--- python_analysis_scripts/edit_analyses/take_out_used_data_from_testing_set.py
+++ python_analysis_scripts/edit_analyses/take_out_used_data_from_testing_set.py
@@ -72,7 +72,7 @@ def run(input_building_data_file, input_testing_data_file,
             sys.stderr.flush()
 
     for i, line in enumerate(input_testing_data_file):
-        if line[0] in random_building_sessions and
+        if line[0] in random_building_sessions and\
             line[1] in random_building_sessions[line[0]]
         
             random_testing_sessions[line[0]][line[1]] = 1
