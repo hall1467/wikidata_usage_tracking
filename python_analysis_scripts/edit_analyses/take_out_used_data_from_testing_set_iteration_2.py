636eaec201ef8539817318b112d470b11d1f688d
hall1467 <hall1467@umn.edu>
2017-12-23 17:17:13 -0600

    Fixed bug

--- python_analysis_scripts/edit_analyses/take_out_used_data_from_testing_set_iteration_2.py
+++ python_analysis_scripts/edit_analyses/take_out_used_data_from_testing_set_iteration_2.py
@@ -88,7 +88,7 @@ def run(input_building_data_file, input_i1_testing_data_file,
 
 
 
-    for i, line in enumerate(input_testing_data_file):
+    for i, line in enumerate(input_i2_testing_data_file):
         
         if (line[0] in random_building_sessions and\
             line[1] in random_building_sessions[line[0]]) or\
