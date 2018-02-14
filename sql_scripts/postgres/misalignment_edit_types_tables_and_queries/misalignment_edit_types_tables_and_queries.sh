70e9ca8960c69d424eb308e5e4cf1bc61350b22d
hall1467 <hall1467@umn.edu>
2018-02-13 22:52:04 -0600

    fixed bugs and for consistency

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/misalignment_edit_types_tables_and_queries.sh
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/misalignment_edit_types_tables_and_queries.sh
@@ -149,9 +149,9 @@ echo "Removing old version of '$results/wasted_edits_error_log.txt' (if it exist
 
 # psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_table_creation.sql
 # psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_ordered_by_entity_year_month_query.sql
-# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_entity_views_5_17.sql
-# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_quality_and_view_class_5_17.sql
-# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_quality_class_5_17.sql
+# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_entity_views_5_17_query.sql
+# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_quality_and_view_class_5_17_query.sql
+# psql wikidata_entities < $base/misalignment_and_edits_table/misalignment_and_edits_edit_proportions_by_quality_class_5_17_query.sql
 
 # python $base/misalignment_and_edits_table/wasted_edit_analysis.py \
 # 	$results/monthly_misalignment_and_edits_ordered_by_entity_year_month.tsv \
