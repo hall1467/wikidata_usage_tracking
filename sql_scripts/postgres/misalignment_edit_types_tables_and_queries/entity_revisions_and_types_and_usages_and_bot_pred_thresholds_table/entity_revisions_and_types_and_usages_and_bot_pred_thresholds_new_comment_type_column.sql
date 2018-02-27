bd472b399e0553206d8e65cb8f9b71ea9178aaf2
hall1467 <hall1467@umn.edu>
2018-02-27 10:53:12 -0600

    rerunning bot prediction and column adding

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
@@ -1,15 +1,15 @@
 ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN reference_manipulation text;
+ADD COLUMN reference_manipulation BOOLEAN;
 ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN sitelink_manipulation text;
+ADD COLUMN sitelink_manipulation BOOLEAN;
 ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN label_description_or_alias_manipulation text;
+ADD COLUMN label_description_or_alias_manipulation BOOLEAN;
 
 UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET reference_manipulation = (CASE WHEN lower(comment) LIKE '%reference%' THEN 'reference' END);
+SET reference_manipulation = (CASE WHEN lower(comment) LIKE '%reference%' THEN TRUE ELSE FALSE END);
 
 UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN 'sitelink' END);
+SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN TRUE ELSE FALSE END);
 
 UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET label_description_or_alias_manipulation = (CASE WHEN lower(comment) LIKE '%alias%' OR lower(comment) LIKE '%label%' OR lower(comment) LIKE '%description%' THEN 'label_description_or_alias' END);
+SET label_description_or_alias_manipulation = (CASE WHEN lower(comment) LIKE '%alias%' OR lower(comment) LIKE '%label%' OR lower(comment) LIKE '%description%' THEN TRUE ELSE FALSE END);
