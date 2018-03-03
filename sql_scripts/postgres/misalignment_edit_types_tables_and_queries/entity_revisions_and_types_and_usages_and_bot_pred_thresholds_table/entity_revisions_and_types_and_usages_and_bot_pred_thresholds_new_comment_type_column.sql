6072658563745df47f90bdb1c700843d0c0e656a
hall1467 <hall1467@umn.edu>
2018-03-03 17:52:18 -0600

    Checking size of table before adding columns

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
@@ -8,8 +8,8 @@ ADD COLUMN label_description_or_alias_manipulation BOOLEAN;
 UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
 SET reference_manipulation = (CASE WHEN lower(comment) LIKE '%reference%' THEN TRUE ELSE FALSE END);
 
-UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN TRUE ELSE FALSE END);
+-- UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+-- SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN TRUE ELSE FALSE END);
 
-UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET label_description_or_alias_manipulation = (CASE WHEN lower(comment) LIKE '%alias%' OR lower(comment) LIKE '%label%' OR lower(comment) LIKE '%description%' THEN TRUE ELSE FALSE END);
+-- UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+-- SET label_description_or_alias_manipulation = (CASE WHEN lower(comment) LIKE '%alias%' OR lower(comment) LIKE '%label%' OR lower(comment) LIKE '%description%' THEN TRUE ELSE FALSE END);
