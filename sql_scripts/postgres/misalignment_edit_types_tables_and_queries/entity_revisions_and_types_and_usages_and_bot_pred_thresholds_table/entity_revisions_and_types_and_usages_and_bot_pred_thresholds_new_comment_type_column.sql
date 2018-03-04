f8b43449992836f401de9d2ab6e5196582b3f65a
hall1467 <hall1467@umn.edu>
2018-03-03 21:32:00 -0600

    Checking size of table before adding columns

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_table/entity_revisions_and_types_and_usages_and_bot_pred_thresholds_new_comment_type_column.sql
@@ -1,15 +1,15 @@
-ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN reference_manipulation BOOLEAN;
-ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN sitelink_manipulation BOOLEAN;
-ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-ADD COLUMN label_description_or_alias_manipulation BOOLEAN;
-
-UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
-SET reference_manipulation = (CASE WHEN lower(comment) LIKE '%reference%' THEN TRUE ELSE FALSE END);
+-- ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+-- ADD COLUMN reference_manipulation BOOLEAN;
+-- ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+-- ADD COLUMN sitelink_manipulation BOOLEAN;
+-- ALTER TABLE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+-- ADD COLUMN label_description_or_alias_manipulation BOOLEAN;
 
 -- UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
--- SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN TRUE ELSE FALSE END);
+-- SET reference_manipulation = (CASE WHEN lower(comment) LIKE '%reference%' THEN TRUE ELSE FALSE END);
+
+UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
+SET sitelink_manipulation = (CASE WHEN lower(comment) LIKE '%sitelink%' THEN TRUE ELSE FALSE END);
 
 -- UPDATE entity_revisions_and_types_and_usages_and_bot_pred_thresholds
 -- SET label_description_or_alias_manipulation = (CASE WHEN lower(comment) LIKE '%alias%' OR lower(comment) LIKE '%label%' OR lower(comment) LIKE '%description%' THEN TRUE ELSE FALSE END);
