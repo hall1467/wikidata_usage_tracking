e34c1eae94bbf15bc702b8617b2497e8bdb69eb2
hall1467 <hall1467@umn.edu>
2018-03-28 16:40:02 -0500

    fixed bug

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
@@ -1,28 +1,27 @@
-# entity_weighted_sums_and_page_views <- read.table(commandArgs()[6], header=TRUE, sep="\t");
+# entity_weighted_sums_and_page_views <- read.table(commandArgs(trailingOnly = TRUE)[1], header=TRUE, sep="\t");
 
-print(commandArgs(trailingOnly = TRUE))
-# summary(entity_weighted_sums_and_page_views)
-# entity_weighted_sums_and_page_views$expected_quality_quantile = ecdf(entity_weighted_sums_and_page_views$page_views)(entity_weighted_sums_and_page_views$page_views);
-# weighted_sum_distribution = ecdf(entity_weighted_sums_and_page_views$weighted_sum);
-# entity_weighted_sums_and_page_views$expected_quality = quantile(weighted_sum_distribution, probs=entity_weighted_sums_and_page_views$expected_quality_quantile);
-# entity_weighted_sums_and_page_views$quality_difference = entity_weighted_sums_and_page_views$expected_quality - entity_weighted_sums_and_page_views$weighted_sum;
-
+summary(entity_weighted_sums_and_page_views)
+entity_weighted_sums_and_page_views$expected_quality_quantile = ecdf(entity_weighted_sums_and_page_views$page_views)(entity_weighted_sums_and_page_views$page_views);
+weighted_sum_distribution = ecdf(entity_weighted_sums_and_page_views$weighted_sum);
+entity_weighted_sums_and_page_views$expected_quality = quantile(weighted_sum_distribution, probs=entity_weighted_sums_and_page_views$expected_quality_quantile);
+entity_weighted_sums_and_page_views$quality_difference = entity_weighted_sums_and_page_views$expected_quality - entity_weighted_sums_and_page_views$weighted_sum;
 
 
-# quality_differences_added_up = sum(entity_weighted_sums_and_page_views$quality_difference);
+
+quality_differences_added_up = sum(entity_weighted_sums_and_page_views$quality_difference);
 
 
-# if (quality_differences_added_up < 0) {
-#     RMSE_with_sign = -sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views))
-# } else {
-#     RMSE_with_sign = sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views)) 
-# };
+if (quality_differences_added_up < 0) {
+    RMSE_with_sign = -sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views))
+} else {
+    RMSE_with_sign = sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views)) 
+};
     
-# output = data.frame(head(entity_weighted_sums_and_page_views, n=1)$year,
-#                     head(entity_weighted_sums_and_page_views, n=1)$month,
-#                     RMSE_with_sign);
+output = data.frame(head(entity_weighted_sums_and_page_views, n=1)$year,
+                    head(entity_weighted_sums_and_page_views, n=1)$month,
+                    RMSE_with_sign);
 
-# write.table(output,'/export/scratch2/wmf/wbc_entity_usage/usage_results/misalignment_edit_types_tables_and_queries/rmse_with_sign.tsv', row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t', append = TRUE);
+write.table(output,'/export/scratch2/wmf/wbc_entity_usage/usage_results/misalignment_edit_types_tables_and_queries/rmse_with_sign.tsv', row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t', append = TRUE);
 
 
 
