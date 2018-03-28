04c285064c11e5ae28aebe6a89d869582a9acf6a
hall1467 <hall1467@umn.edu>
2018-03-28 15:19:31 -0500

    fixed bugs

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
@@ -1,6 +1,3 @@
-
-library(data.table);
-
 entity_weighted_sums_and_page_views <- read.table(commandArgs()[1], header=TRUE, sep="\t");
 
 entity_weighted_sums_and_page_views$expected_quality_quantile = ecdf(entity_weighted_sums_and_page_views$page_views)(entity_weighted_sums_and_page_views$page_views);
