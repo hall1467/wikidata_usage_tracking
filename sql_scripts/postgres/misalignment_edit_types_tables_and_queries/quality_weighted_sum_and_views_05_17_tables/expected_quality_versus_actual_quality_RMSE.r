6ff30e3e552c876a1b0ec5e7ce28b89b02e13e76
hall1467 <hall1467@umn.edu>
2018-03-29 11:06:58 -0500

    Fixed bug

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/expected_quality_versus_actual_quality_RMSE.r
@@ -5,23 +5,35 @@ colnames(entity_weighted_sums_and_page_views) <- c('page_title','yyyy','mm', 'we
 entity_weighted_sums_and_page_views$expected_quality_quantile = ecdf(entity_weighted_sums_and_page_views$page_views)(entity_weighted_sums_and_page_views$page_views);
 weighted_sum_distribution = ecdf(entity_weighted_sums_and_page_views$weighted_sum);
 entity_weighted_sums_and_page_views$expected_quality = quantile(weighted_sum_distribution, probs=entity_weighted_sums_and_page_views$expected_quality_quantile);
-entity_weighted_sums_and_page_views$quality_difference = entity_weighted_sums_and_page_views$expected_quality - entity_weighted_sums_and_page_views$weighted_sum;
+entity_weighted_sums_and_page_views$quality_difference = entity_weighted_sums_and_page_views$weighted_sum - entity_weighted_sums_and_page_views$expected_quality;
+
+entity_weighted_sums_and_page_views$quality_difference_sign = if (entity_weighted_sums_and_page_views$quality_difference < 0) -1 else 1;
+
+mean_quality_difference = mean(entity_weighted_sums_and_page_views$quality_difference)
+mean_quality_differences_squared = sum((entity_weighted_sums_and_page_views$quality_difference^2)*entity_weighted_sums_and_page_views$quality_difference_sign)
 
 
-
-quality_differences_added_up = sum(entity_weighted_sums_and_page_views$quality_difference);
-
-
-if (quality_differences_added_up < 0) {
-    RMSE_with_sign = -sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views))
+if (mean_quality_difference < 0) {
+    RMSE_with_sign = -sqrt(sum(entity_weighted_sums_and_page_views$quality_difference^2)/nrow(entity_weighted_sums_and_page_views))
 } else {
-    RMSE_with_sign = sqrt(quality_differences_added_up^2/nrow(entity_weighted_sums_and_page_views)) 
+    RMSE_with_sign = sqrt(sum(entity_weighted_sums_and_page_views$quality_difference^2)/nrow(entity_weighted_sums_and_page_views)) 
 };
+
+
+if (mean_quality_differences_squared < 0) {
+    RMSE_with_sign_for_squared_sum = -sqrt(sum(entity_weighted_sums_and_page_views$quality_difference^2)/nrow(entity_weighted_sums_and_page_views))
+} else {
+    RMSE_with_sign_for_squared_sum = sqrt(sum(entity_weighted_sums_and_page_views$quality_difference^2)/nrow(entity_weighted_sums_and_page_views)) 
+};
+
+
     
 output = data.frame()
 output = rbind(output, c(entity_weighted_sums_and_page_views[1,2], 
                          entity_weighted_sums_and_page_views[1,3], 
-                         RMSE_with_sign))
+                         RMSE_with_sign,
+                         RMSE_with_sign_for_squared_sum,
+                         mean_quality_difference))
 
 write.table(output[1,],'/export/scratch2/wmf/wbc_entity_usage/usage_results/misalignment_edit_types_tables_and_queries/rmse_with_sign.tsv', row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t', append = TRUE);
 
