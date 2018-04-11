1f975d03068e17ab5c4786ddfb155687e320cecb
hall1467 <hall1467@umn.edu>
2018-04-10 22:18:42 -0500

    Testing

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/2013_2014_revision_alignment.r
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/quality_weighted_sum_and_views_05_17_tables/2013_2014_revision_alignment.r
@@ -54,7 +54,8 @@ for (monthly_distribution_and_edits in c(
                     list('/export/scratch2/wmf/wbc_entity_usage/usage_results/misalignment_edit_types_tables_and_queries/input_for_rmse_split_directory/2014/input_for_RMSE_sub_19', 'test')
 )){
   
-     distribution = monthly_distribution_and_edits[[2]]
+     names(monthly_distribution_and_edits) = c('distribution', 'revisions')
+     distribution = monthly_distribution_and_edits['distribution']
      revisions = monthly_distribution_and_edits[1]
      print(distribution)
      # print(revisions)
