526330f10aaf37f004099470a7a9e5ea8ec642c7
hall1467 <hall1467@umn.edu>
2018-09-29 22:06:43 -0500

    updated API script

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/obtain_entity_data_from_api.py
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/obtain_entity_data_from_api.py
@@ -1,12 +1,13 @@
 """
-Takes in revisions (and associated data) and returns the gender for 
-entities (items or properties) that have one and null otherwise.
+Takes in revisions (and associated data) and returns gender, instance_of, and
+subclass_of information for entities (items or properties) that have one and 
+null otherwise.
 
 Usage:
-    obtain_entity_gender_data (-h|--help)
-    obtain_entity_gender_data <input> <output>
-                              [--debug]
-                              [--verbose]
+    obtain_entity_data_from_api (-h|--help)
+    obtain_entity_data_from_api <input> <output>
+                                [--debug]
+                                [--verbose]
 
 Options:
     -h, --help  This help message is printed
@@ -24,6 +25,7 @@ from collections import defaultdict
 import mysqltsv
 import sys
 import mwapi
+import json
 
 
 logger = logging.getLogger(__name__)
@@ -45,22 +47,7 @@ def main(argv=None):
         types=[str, int, str, str, str, float, float, float, str, str, str, 
         str])
 
-
-    output_file = mysqltsv.Writer(
-        open(args['<output>'], "w"), 
-        headers=['page_title',
-                 'namespace',
-                 'comment',
-                 'edit_type',
-                 'agent_type',
-                 'rev_id',
-                 'weighted_sum',
-                 'expected_quality',
-                 'expected_quality_quantile',
-                 'page_views',
-                 'yyyy',
-                 'mm',
-                 'gender'])
+    output_file = open(args['<output>'], "w")
 
     verbose = args['--verbose']
 
@@ -95,16 +82,20 @@ def run(input_file, input_second_iteration_file, output_file, verbose):
 
     accessed_gender_for_revisions_count = 0
     gender_dict = defaultdict(str)
+    instance_of_dict = defaultdict(list)
+    subclass_of_dict = defaultdict(list)
     
     # Make API calls and process results
     for in_list in entity_id_lists:
 
+        if accessed_gender_for_revisions_count > 3000:
+            break
         accessed_gender_for_revisions_count += len(in_list)
 
 
         if verbose and accessed_gender_for_revisions_count % 100 == 0 and \
             accessed_gender_for_revisions_count != 0:
-            sys.stderr.write("Getting gender for revision: {0}\n"
+            sys.stderr.write("Getting API info for revision: {0}\n"
                     .format(accessed_gender_for_revisions_count))  
             sys.stderr.flush()
 
@@ -135,26 +126,76 @@ def run(input_file, input_second_iteration_file, output_file, verbose):
                 gender_dict[entity] = gender_claim
 
 
+            if 'claims' in api_claims_result['entities'][entity] and \
+                'P31' in api_claims_result['entities'][entity]['claims'] and \
+                'mainsnak' in api_claims_result['entities'][entity]['claims']\
+                    ['P31'][0] and \
+                'datavalue' in api_claims_result['entities'][entity]\
+                    ['claims']['P31'][0]['mainsnak'] and \
+                'value' in api_claims_result['entities'][entity]['claims']\
+                    ['P31'][0]['mainsnak']['datavalue'] and \
+                'id' in api_claims_result['entities'][entity]['claims']['P31']\
+                    [0]['mainsnak']['datavalue']['value']:
+                instance_of_dict_claim = \
+                    api_claims_result['entities'][entity]['claims']['P31'][0]\
+                        ['mainsnak']['datavalue']['value']['id']
+                print(entity)
+                instance_of_dict[entity].append(instance_of_dict_claim)
+
+
+            if 'claims' in api_claims_result['entities'][entity] and \
+                'P279' in api_claims_result['entities'][entity]['claims'] and \
+                'mainsnak' in api_claims_result['entities'][entity]['claims']\
+                    ['P279'][0] and \
+                'datavalue' in api_claims_result['entities'][entity]\
+                    ['claims']['P279'][0]['mainsnak'] and \
+                'value' in api_claims_result['entities'][entity]['claims']\
+                    ['P279'][0]['mainsnak']['datavalue'] and \
+                'id' in api_claims_result['entities'][entity]['claims']['P279']\
+                    [0]['mainsnak']['datavalue']['value']:
+                subclass_of_dict_claim = \
+                    api_claims_result['entities'][entity]['claims']['P279'][0]\
+                        ['mainsnak']['datavalue']['value']['id']
+                subclass_of_dict[entity].append(subclass_of_dict_claim)
+
+
+
     for i, line in enumerate(input_second_iteration_file):
+
+        
+        if verbose and i % 10000 == 0 and i != 0:
+            sys.stderr.write("Merging results: {0}\n".format(i))  
+            sys.stderr.flush()
+
         gender_value = None
+        instance_of_value = []
+        subclass_of_value = []
+
         if line[0] in gender_dict:
             gender_value = gender_dict[line[0]]
+        if line[0] in instance_of_dict:
+            instance_of_value = instance_of_dict[line[0]]
+        if line[0] in subclass_of_dict:
+            subclass_of_value = subclass_of_dict[line[0]]
+            
 
-
-        output_file.write([
-            line[0],
-            line[1],
-            line[2],
-            line[3],
-            line[4],
-            line[5],
-            line[6],
-            line[7],
-            line[8],
-            line[9],
-            line[10],
-            line[11],
-            gender_value])
+        output_file.write(json.dumps({
+                    'page_title' : line[0],
+                    'namespace' : line[1],
+                    'edit_type': line[2],
+                    'agent_type': line[3],
+                    'rev_id': line[4],
+                    'weighted_sum': line[5],
+                    'expected_quality': line[6],
+                    'expected_quality_quantile': line[7],
+                    'page_views': line[8],
+                    'yyyy': line[9],
+                    'mm': line[10],
+                    'quality_difference': line[11],
+                    'gender': gender_value,
+                    'instance_of': instance_of_value,
+                    'subclass_of': subclass_of_value
+                }) + "\n")
 
 
 main()
