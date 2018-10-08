cea0a51069fc5a03094ee73fa495c76e0e867501
hall1467 <hall1467@umn.edu>
2018-10-07 20:28:59 -0500

    Adding new script

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/misalignment.sh
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/misalignment.sh
@@ -243,29 +243,29 @@ set monthly_revisions_directory = $results/monthly_revisions_directory
 # 	> $results/revision_parent_data_2016_2017_with_quality.json
 
 
-python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
-	$results/revision_parent_data_2013_2014_with_quality.json \
-	$results/parent_data_with_extracted_weighted_score_2013_2014.tsv \
-	--verbose > & \
-	$results/parent_data_with_extracted_weighted_score_2013_2014_error_log.txt
+# python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
+# 	$results/revision_parent_data_2013_2014_with_quality.json \
+# 	$results/parent_data_with_extracted_weighted_score_2013_2014.tsv \
+# 	--verbose > & \
+# 	$results/parent_data_with_extracted_weighted_score_2013_2014_error_log.txt
 
-python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
-	$results/revision_parent_data_2014_2015_with_quality.json \
-	$results/parent_data_with_extracted_weighted_score_2014_2015.tsv \
-	--verbose > & \
-	$results/parent_data_with_extracted_weighted_score_2014_2015_error_log.txt
+# python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
+# 	$results/revision_parent_data_2014_2015_with_quality.json \
+# 	$results/parent_data_with_extracted_weighted_score_2014_2015.tsv \
+# 	--verbose > & \
+# 	$results/parent_data_with_extracted_weighted_score_2014_2015_error_log.txt
 
-python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
-	$results/revision_parent_data_2015_2016_with_quality.json \
-	$results/parent_data_with_extracted_weighted_score_2015_2016.tsv \
-	--verbose > & \
-	$results/parent_data_with_extracted_weighted_score_2015_2016_error_log.txt
+# python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
+# 	$results/revision_parent_data_2015_2016_with_quality.json \
+# 	$results/parent_data_with_extracted_weighted_score_2015_2016.tsv \
+# 	--verbose > & \
+# 	$results/parent_data_with_extracted_weighted_score_2015_2016_error_log.txt
 
-python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
-	$results/revision_parent_data_2016_2017_with_quality.json \
-	$results/parent_data_with_extracted_weighted_score_2016_2017.tsv \
-	--verbose > & \
-	$results/parent_data_with_extracted_weighted_score_2016_2017_error_log.txt
+# python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent.py \
+# 	$results/revision_parent_data_2016_2017_with_quality.json \
+# 	$results/parent_data_with_extracted_weighted_score_2016_2017.tsv \
+# 	--verbose > & \
+# 	$results/parent_data_with_extracted_weighted_score_2016_2017_error_log.txt
 
 
 # python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
@@ -293,32 +293,32 @@ python $base/longitudinal_misalignment_tables/extract_weighted_score_from_parent
 # 	$results/all_revisions_with_api_entity_data_2016_2017_error_log.txt
 
 
-# python $base/longitudinal_misalignment_tables/merge_parent_data.py \
-# 	$results/all_revisions_with_api_entity_data_2013_2014.tsv \
-# 	$results/revision_parent_data_2013_2014_with_quality.json \
-# 	$results/all_revisions_with_api_and_parent_entity_data_2013_2014.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_and_parent_entity_data_error_log_2013_2014.tsv
+python $base/longitudinal_misalignment_tables/merge_parent_data.py \
+	$results/all_revisions_with_api_entity_data_2013_2014.tsv \
+	$results/revision_parent_data_2013_2014_with_quality.json \
+	$results/all_revisions_with_api_and_parent_entity_data_2013_2014.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_and_parent_entity_data_error_log_2013_2014.tsv
 
-# python $base/longitudinal_misalignment_tables/merge_parent_data.py \
-# 	$results/all_revisions_with_api_entity_data_2014_2015.tsv \
-# 	$results/revision_parent_data_2014_2015_with_quality.json \
-# 	$results/all_revisions_with_api_and_parent_entity_data_2014_2015.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_and_parent_entity_data_error_log_2014_2015.tsv
+python $base/longitudinal_misalignment_tables/merge_parent_data.py \
+	$results/all_revisions_with_api_entity_data_2014_2015.tsv \
+	$results/revision_parent_data_2014_2015_with_quality.json \
+	$results/all_revisions_with_api_and_parent_entity_data_2014_2015.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_and_parent_entity_data_error_log_2014_2015.tsv
 
-# python $base/longitudinal_misalignment_tables/merge_parent_data.py \
-# 	$results/all_revisions_with_api_entity_data_2015_2016.tsv \
-# 	$results/revision_parent_data_2015_2016_with_quality.json \
-# 	$results/all_revisions_with_api_and_parent_entity_data_2015_2016.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_and_parent_entity_data_error_log_2015_2016.tsv \
+python $base/longitudinal_misalignment_tables/merge_parent_data.py \
+	$results/all_revisions_with_api_entity_data_2015_2016.tsv \
+	$results/revision_parent_data_2015_2016_with_quality.json \
+	$results/all_revisions_with_api_and_parent_entity_data_2015_2016.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_and_parent_entity_data_error_log_2015_2016.tsv \
 
-# python $base/longitudinal_misalignment_tables/merge_parent_data.py \
-# 	$results/all_revisions_with_api_entity_data_2016_2017.tsv \
-# 	$results/revision_parent_data_2016_2017_with_quality.json \
-# 	$results/all_revisions_with_api_and_parent_entity_data_2016_2017.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_and_parent_entity_data_error_log_2016_2017.tsv \
+python $base/longitudinal_misalignment_tables/merge_parent_data.py \
+	$results/all_revisions_with_api_entity_data_2016_2017.tsv \
+	$results/revision_parent_data_2016_2017_with_quality.json \
+	$results/all_revisions_with_api_and_parent_entity_data_2016_2017.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_and_parent_entity_data_error_log_2016_2017.tsv \
 
 
