3e992473a60912a1857ee8fc485cbe9a85c58438
hall1467 <hall1467@umn.edu>
2018-10-16 16:45:59 -0500

    rerunning

--- sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/misalignment.sh
+++ sql_scripts/postgres/misalignment_edit_types_tables_and_queries/longitudinal_misalignment_tables/misalignment.sh
@@ -37,29 +37,29 @@ set monthly_revisions_directory = $results/monthly_revisions_directory
 # More recently have been moving into directories by year and manually running each to speed up this process. 
 # Otherwise takes 18 hours for the below loop
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2012/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2012_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2012/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2012_error_metrics.tsv
+end
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2013/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2013_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2013/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2013_error_metrics.tsv
+end
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2014/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2014_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2014/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2014_error_metrics.tsv
+end
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2015/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2015_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2015/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2015_error_metrics.tsv
+end
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2016/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2016_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2016/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2016_error_metrics.tsv
+end
 
-# foreach input_RMSE_file ($input_for_rmse_split_directory/2017/input_for_RMSE_sub*)
-# 	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2017_error_metrics.tsv
-# end
+foreach input_RMSE_file ($input_for_rmse_split_directory/2017/input_for_RMSE_sub*)
+	Rscript $base/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r $input_RMSE_file $results/2017_error_metrics.tsv
+end
 
 
 # \copy (SELECT page_title, revision_id, revision_user, comment, namespace, revision_timestamp, year, month, bot_user_id, change_tag_revision_id, number_of_revisions, page_views, edit_type, year_month_page_title, bot_prediction_threshold, session_start, misalignment_matching_year, misalignment_matching_month, edit_type_updated, reference_manipulation, sitelink_manipulation, label_description_or_alias_manipulation, quality_class, views_class FROM misalignment_and_edits where revision_timestamp >= 20130500000000 and revision_timestamp < 20140500000000 AND page_views IS NOT NULL) TO '/export/scratch2/wmf/wbc_entity_usage/usage_results/misalignment_edit_types_tables_and_queries/used_misalignment_and_edits_may_2013_to_2014.tsv';
@@ -193,10 +193,10 @@ set monthly_revisions_directory = $results/monthly_revisions_directory
 # 	$results/extract_weighted_score_2016_to_2017_error_log.txt
 
 
-# Rscript $base/longitudinal_misalignment_tables/2013_2014_revision_alignment.r
-# Rscript $base/longitudinal_misalignment_tables/2014_2015_revision_alignment.r
-# Rscript $base/longitudinal_misalignment_tables/2015_2016_revision_alignment.r
-# Rscript $base/longitudinal_misalignment_tables/2016_2017_revision_alignment.r
+Rscript $base/longitudinal_misalignment_tables/2013_2014_revision_alignment.r
+Rscript $base/longitudinal_misalignment_tables/2014_2015_revision_alignment.r
+Rscript $base/longitudinal_misalignment_tables/2015_2016_revision_alignment.r
+Rscript $base/longitudinal_misalignment_tables/2016_2017_revision_alignment.r
 
 
 # python $base/longitudinal_misalignment_tables/obtain_parent_data_from_api.py \
@@ -268,29 +268,29 @@ set monthly_revisions_directory = $results/monthly_revisions_directory
 # 	$results/parent_data_with_extracted_weighted_score_2016_2017_error_log.txt
 
 
-# python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
-# 	$results/all_revisions_quality_differences_2013_2014.tsv \
-# 	$results/all_revisions_with_api_entity_data_2013_2014.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_entity_data_2013_2014_error_log.txt
+python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
+	$results/all_revisions_quality_differences_2013_2014.tsv \
+	$results/all_revisions_with_api_entity_data_2013_2014.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_entity_data_2013_2014_error_log.txt
 
-# python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
-# 	$results/all_revisions_quality_differences_2014_2015.tsv \
-# 	$results/all_revisions_with_api_entity_data_2014_2015.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_entity_data_2014_2015_error_log.txt
+python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
+	$results/all_revisions_quality_differences_2014_2015.tsv \
+	$results/all_revisions_with_api_entity_data_2014_2015.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_entity_data_2014_2015_error_log.txt
 
-# python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
-# 	$results/all_revisions_quality_differences_2015_2016.tsv \
-# 	$results/all_revisions_with_api_entity_data_2015_2016.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_entity_data_2015_2016_error_log.txt
+python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
+	$results/all_revisions_quality_differences_2015_2016.tsv \
+	$results/all_revisions_with_api_entity_data_2015_2016.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_entity_data_2015_2016_error_log.txt
 
-# python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
-# 	$results/all_revisions_quality_differences_2016_2017.tsv \
-# 	$results/all_revisions_with_api_entity_data_2016_2017.tsv \
-# 	--verbose > & \
-# 	$results/all_revisions_with_api_entity_data_2016_2017_error_log.txt
+python $base/longitudinal_misalignment_tables/obtain_entity_data_from_api.py \
+	$results/all_revisions_quality_differences_2016_2017.tsv \
+	$results/all_revisions_with_api_entity_data_2016_2017.tsv \
+	--verbose > & \
+	$results/all_revisions_with_api_entity_data_2016_2017_error_log.txt
 
 
 # python $base/longitudinal_misalignment_tables/merge_parent_data.py \
