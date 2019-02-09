dc62857df16d89596fd3f8bc6753357c6522c6b8
hall1467 <hall1467@umn.edu>
2019-02-08 22:14:35 -0600

    Forgot to make one more

--- sql_scripts/postgres/longitudinal_misalignment_tables/interesting_subset_revisions_tables/us_items_12_29_18_revisions_table_creation.sql
+++ sql_scripts/postgres/longitudinal_misalignment_tables/interesting_subset_revisions_tables/us_items_12_29_18_revisions_table_creation.sql
@@ -4,6 +4,6 @@ CREATE TABLE us_items_12_29_18_revisions AS (
 	items_with_one_coordinate_location_processed_12_29_18
 	INNER JOIN
 	revisions_final
-	ON revisions_final.page_title = us_items_12_29_18.page_title
+	ON revisions_final.page_title = items_with_one_coordinate_location_processed_12_29_18.page_title
 	WHERE fip = 'NULL'
 );
