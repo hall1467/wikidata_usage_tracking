936e07de6b25160ec1c2f1731b5f1543865337f8
hall1467 <hall1467@umn.edu>
2019-02-16 14:13:11 -0600

    testing again

--- sql_scripts/postgres/longitudinal_misalignment_tables/identify_type_of_work_being_done_in_revision.py
+++ sql_scripts/postgres/longitudinal_misalignment_tables/identify_type_of_work_being_done_in_revision.py
@@ -127,10 +127,7 @@ def run(input_original_file, input_preds_file, output_file, verbose):
 
         # Compute actual minus expected quality
         quality_difference = None
-        print(line[13])
         if line[13] == 'True' and line[14]:
-            print(line[14])
-            print(line[8])
             quality_difference = float(line[14]) - float(line[8])
 
             
