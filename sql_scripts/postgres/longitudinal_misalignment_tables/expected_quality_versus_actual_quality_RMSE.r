8daccf5262b5e4aefc2357ce354080dc97883feb
hall1467 <hall1467@umn.edu>
2019-03-19 23:03:28 -0500

    Updated error metric script

--- sql_scripts/postgres/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r
+++ sql_scripts/postgres/longitudinal_misalignment_tables/expected_quality_versus_actual_quality_RMSE.r
@@ -9,29 +9,18 @@ weighted_sum_distribution = ecdf(entity_weighted_sums_and_page_views$weighted_su
 entity_weighted_sums_and_page_views$expected_quality = quantile(weighted_sum_distribution, probs=entity_weighted_sums_and_page_views$expected_quality_quantile);
 entity_weighted_sums_and_page_views$quality_difference = entity_weighted_sums_and_page_views$weighted_sum - entity_weighted_sums_and_page_views$expected_quality;
 
+entity_weighted_sums_and_page_views$quality_quantile = ecdf(entity_weighted_sums_and_page_views$weighted_sum)(entity_weighted_sums_and_page_views$weighted_sum)
+
 entity_weighted_sums_and_page_views$quality_difference_sign = sign(entity_weighted_sums_and_page_views$quality_difference);
 
-me = sum(entity_weighted_sums_and_page_views$quality_difference)/nrow(entity_weighted_sums_and_page_views)
-mae = sum(abs(entity_weighted_sums_and_page_views$quality_difference))/nrow(entity_weighted_sums_and_page_views)
-median_error = median(entity_weighted_sums_and_page_views$quality_difference)
-median_absolute_error = median(abs(entity_weighted_sums_and_page_views$quality_difference))
-mad = median(abs(entity_weighted_sums_and_page_views$quality_difference - median(entity_weighted_sums_and_page_views$quality_difference)))
-rmse = sqrt(sum(entity_weighted_sums_and_page_views$quality_difference^2)/nrow(entity_weighted_sums_and_page_views))
+mae_quantile = mean(abs(entity_weighted_sums_and_page_views$quality_quantile - entity_weighted_sums_and_page_views$expected_quality_quantile))
+mae = mean(abs(entity_weighted_sums_and_page_views$quality_difference))
 
-sum_quality_differences_squared = sum((entity_weighted_sums_and_page_views$quality_difference^2)*entity_weighted_sums_and_page_views$quality_difference_sign)
-sum_quality_differences_squared_sign = sign(sum_quality_differences_squared)
-rmse_with_sign = sqrt(abs(sum_quality_differences_squared)/nrow(entity_weighted_sums_and_page_views))*sum_quality_differences_squared_sign
-    
 output = data.frame()
 output = rbind(output, c(entity_weighted_sums_and_page_views[1,2], 
                          entity_weighted_sums_and_page_views[1,3], 
-                         me,
-                         mae,
-                         median_error,
-                         median_absolute_error,
-                         mad,
-                         rmse,
-                         rmse_with_sign))
+                         mae_quantile,
+                         mae))
 
 write.table(output[1,], output_file, row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t', append = TRUE);
 
